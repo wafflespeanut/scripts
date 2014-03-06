3e71c20047c623ce97e65ce3063d9be063fc0562
Wafflespeanut <wafflescrazypeanut@gmail.com>
2014-03-06 15:11:50 +0530

    My dump

---
+++ ROUGH/Largest Substring in Order - Elegant.py
@@ -0,0 +1,10 @@
+temp = s[0]
+long = s[0]
+for i in range(1, len(s)):
+    if s[i] >= temp[-1]:
+        temp += s[i]
+        if len(temp) > len(long):
+            long = temp
+    else:
+        temp = s[i]
+print 'long substring in alphabetical order is:', long
---
+++ ROUGH/Largest Substring in Order - Exhausted.py
@@ -0,0 +1,17 @@
+out=''
+temp=''
+for char in s:
+    if temp=='':
+        temp=char
+    if temp[-1]<=char:
+        temp+=char
+    elif temp[-1]>char:
+        if len(out)<len(temp):
+            out=temp
+            temp=char
+        else:
+            temp=char
+if len(temp)>len(out):
+           out=temp
+    
+print "Longest substring in alphabetical order is: " +out
