c098098800be50ea11094913602eb5014dbd6a1d
Wafflespeanut <wafflescrazypeanut@gmail.com>
2014-05-02 18:43:28 +0530

    Prime sieve

---
+++ Collection/Sieve of Eratosthenes.py
@@ -0,0 +1,28 @@
+import timeit
+
+def listlook(n):
+    sidekick=[]
+    primes=[]
+    for i in range(2,n+1):
+        if i not in sidekick:
+            primes.append(i)
+            sidekick.extend(range(i*i,n+1,i))
+
+def exhausted(n):
+    sidekick=[]
+    primes=[]
+    for i in range(2,n+1):
+        for j in range(2,n+1):
+            sidekick.append(i*j)
+        if i not in sidekick:
+            primes.append(i)
+
+start=timeit.default_timer()
+listlook(300)
+stop=timeit.default_timer()
+print "List-looking took " +str(stop-start) +" seconds"
+
+start=timeit.default_timer()
+exhausted(300)
+stop=timeit.default_timer()
+print "Brute-force took " +str(stop-start)+ " seconds"
