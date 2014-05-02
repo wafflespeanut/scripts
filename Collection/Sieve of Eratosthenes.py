814d0e8859b946a731e0318cda3753bc33f3c28d
Wafflespeanut <wafflescrazypeanut@gmail.com>
2014-05-02 18:54:09 +0530

    Prime sieve

--- Collection/Sieve of Eratosthenes.py
+++ Collection/Sieve of Eratosthenes.py
@@ -1,13 +1,5 @@
 import timeit
 
-def listlook(n):
-    sidekick=[]
-    primes=[]
-    for i in range(2,n+1):
-        if i not in sidekick:
-            primes.append(i)
-            sidekick.extend(range(i*i,n+1,i))
-
 def exhausted(n):
     sidekick=[]
     primes=[]
@@ -16,6 +8,14 @@ def exhausted(n):
             sidekick.append(i*j)
         if i not in sidekick:
             primes.append(i)
+
+def listlook(n):
+    sidekick=[]
+    primes=[]
+    for i in range(2,n+1):
+        if i not in sidekick:
+            primes.append(i)
+            sidekick.extend(range(i*i,n+1,i))
 
 start=timeit.default_timer()
 listlook(300)
