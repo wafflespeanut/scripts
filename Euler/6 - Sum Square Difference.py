b9b62c65b6c327348e39e1eebb6b34dc2c22057d
Wafflespeanut <wafflescrazypeanut@gmail.com>
2014-08-10 00:14:08 +0530

    Added stuff

---
+++ Euler/6 - Sum Square Difference.py
@@ -0,0 +1,13 @@
+def sumSquare(n):
+    temp=0
+    for i in range(n+1):
+        temp+=i**2
+    return temp
+
+def squareSum(n):
+    temp=0
+    for i in range(n+1):
+        temp+=i
+    return temp**2
+
+# print "The difference between sum of squares and square of sums of first 100 natural numbers: " +str(squareSum(100)-sumSquare(100))
