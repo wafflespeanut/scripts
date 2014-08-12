2ca724e2a17dcf6d3fc839c16cd6e0d55540e34e
Wafflespeanut <wafflescrazypeanut@gmail.com>
2014-08-12 14:21:38 +0530

    Added something...

---
+++ Euler/12 - Highly Divisible Triangular Number.py
@@ -0,0 +1,19 @@
+def factors(n):    
+    return list(set(reduce(list.__add__, 
+                ([i,n/i] for i in range(1,int(n**0.5)+1) if not n%i))))
+
+def triangle(r):
+    i=1; s=0; a=[]
+    while i<=r: s+=i; i+=1
+    return s
+
+def find(n):
+    i=1
+    while True:
+        t=triangle(i); f=len(factors(t))
+        if f>=n: print("No: %i\tFactors: %s")%(t,f); break
+        i+=1
+
+# n=500
+# print "The first triangle number to have over " +str(n)+ " divisors..."
+# find(n)
