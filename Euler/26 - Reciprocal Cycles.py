deee347b85860eb7045e2ed61afcb05fa0b6b7bc
Wafflespeanut <wafflescrazypeanut@gmail.com>
2014-08-24 21:32:31 +0530

    Minor correction

--- Euler/26 - Reciprocal Cycles.py
+++ Euler/26 - Reciprocal Cycles.py
@@ -2,16 +2,16 @@ execfile("7 - 10001st Prime.py")
 
 def primes(n): return [3]+sieve(n)[3:]
 
-def recur(n,i):
+def recur(i):
     for k in range(1,i):
         if 1==10**k %i: return k
     return 0
 
-def maxreci(n,r):
+def maxreci(r):
     plist=primes(r); t=0; final=[]
     for i in plist:
-        if recur(n,i)>t: t=recur(n,i); final=[i,t]
+        if recur(i)>t: t=recur(i); final=[i,t]
     return final
 
-#n=1; r=1000; t=maxreci(n,r)
+#n=1; r=1000; t=maxreci(r)
 #print "The value which contains the longest recurring cycle in unit fractions is: " +str(t[0])+ " (" +str(t[1])+ " numbers recurred)"
