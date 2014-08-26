2cfee234893e29102b739966ce11a450bc6aac5a
Wafflespeanut <wafflescrazypeanut@gmail.com>
2014-08-26 21:38:32 +0530

    Solved

--- Euler/28 - Number Spiral Diagonals.py
+++ Euler/28 - Number Spiral Diagonals.py
@@ -2,8 +2,8 @@ def spiral(n):
     c=1; diag=1; i=1
     while i<n**2:
         r=1
-        while r<=4:
-            if c%2==1: i+=c+1; diag+=i
+        while r<=4:     # each level has 4 rounds
+            if c%2==1: i+=c+1; diag+=i      # odd number spacing in diagonal numbers
             r+=1
         c+=1
     return diag
