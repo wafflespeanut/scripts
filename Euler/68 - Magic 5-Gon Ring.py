512265b7c4f18812948df2f4314deb11a934be1e
Wafflespeanut <wafflescrazypeanut@gmail.com>
2014-10-06 22:32:50 +0530

    Weirdo solution...

--- Euler/68 - Magic 5-Gon Ring.py
+++ Euler/68 - Magic 5-Gon Ring.py
@@ -5,12 +5,10 @@ def sets(n):        # Gives the possible sets for working out by hand!
         c=0
         for i in l:
             if i>n: c+=1
-        if c>1: return False
-        return True
+        return c==1
     l=range(1,n*2+1); s=(2*sum(l[:n])+sum(l[n:]))/n
     c=list(combinations(l,3))
-    a=[list(i) for i in c if sum(i)==s and check(i)]
-    return a
+    return [i for i in c if sum(i)==s and check(i)]
 
 def couple(l):
     s=''
@@ -18,5 +16,5 @@ def couple(l):
         for j in i: s+=str(j)
     return s
 
-#s=[[6,5,3],[10,3,1],[9,1,4],[8,4,2],[7,2,5]]   # After matching stuff in paper!
+#s=[[6,5,3],[10,3,1],[9,1,4],[8,4,2],[7,2,5]]   # That's what you get in paper!
 #print "The maximum 16-digit string is:",couple(s)
