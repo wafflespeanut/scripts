f182cf5fa266a6832024d0b95afbbef47969cb8d
Wafflespeanut <wafflescrazypeanut@gmail.com>
2014-10-12 11:31:48 +0530

    Still bruteforcing...

--- Euler/71 - Ordered Fractions.py
+++ Euler/71 - Ordered Fractions.py
@@ -1,16 +1,10 @@
 execfile("5 - Smallest Multiple.py")
 
-def HCF(f): g=gcd(f[0],f[1]); return [f[0]/g,f[1]/g]
-
-def frac(f1,f2):
-    if float(f1[0])/f1[1]>float(f2[0])/f2[1]: return 1
-    elif float(f1[0])/f1[1]<float(f2[0])/f2[1]: return -1
-    else: return 0
-
-def fracgen(r):
-    a=[]
-    for n in range(1,r):
-        for d in range(1,r+1):
-            if gcd(n,d)==1: a.append([n,d])
-    a.sort(frac)
+def find(r,f):
+    def frac(f): return float(f[0])/f[1]
+    a=[1,r]; k=float(f[0])/f[1]
+    for d in range(1,r+1):
+        for n in range(1,d):
+            if gcd(n,d)==1:
+                if frac([n,d])>frac(a) and frac([n,d])<k: a=[n,d]
     return a
