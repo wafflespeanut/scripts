5abfd148d93d40283228036167477675f194eeed
Wafflespeanut <wafflescrazypeanut@gmail.com>
2014-12-08 10:00:33 +0530

    For downloading stuff...

--- Scrap/Tiny Utilities/Download JPGs.py
+++ Scrap/Tiny Utilities/Download JPGs.py
@@ -3,11 +3,11 @@ from os import mkdir
 import lxml.html
 from random import choice
 
-o="C:\\Downloads\\"; link=""
+o="C:\\Downloads\\"; links=[]
 
 # I used to download comics with this...
 
-def name():     # Just to put random names to the files!
+def name():     # Just to put random names to the folders!
     s='c'
     for i in range(3): s+=str(choice(range(10)))
     for i in range(3): s+=chr(choice(range(97,123)))
@@ -19,9 +19,10 @@ def urls(link):
     for l in dom.xpath('//a/@href'):
         if '#' not in l and 'jpg' in l: return l
 
-def getimg(link):       # Grabbing a list of images
-    out=o+name()+'//'; mkdir(out)
-    for i in range(15):
-        l=link+str(i)+'/'
-        print "Downloading",l,"..."
-        urllib.urlretrieve(urls(l),out+str(i+1)+'.jpg')
+def getimg():       # Grabbing a sequential list of 15 images
+    for link in links:
+        out=o+name()+'//'; mkdir(out)
+        for i in range(15):
+            l=link+str(i)+'/'
+            print "Downloading",l,"..."
+            urllib.urlretrieve(urls(l),out+str(i+1)+'.jpg')
---
+++ Scrap/Tiny Utilities/Download MP3s.py
@@ -0,0 +1,30 @@
+from urllib import *
+from os import *
+import lxml.html
+
+out="C:\\Users\\Waffles Crazy Peanut\\Desktop\\"; links=[]
+
+# I used to download embedded MP3 soundtracks with this...
+
+def urls(link):
+    a=set(); connection=urlopen(link)
+    dom=lxml.html.fromstring(connection.read())
+    for l in dom.xpath('//a/@href'):
+        if '#' not in l and 'mp3' in l: a.update([l])
+    return list(a)
+
+def get():
+    for link in links:
+        k=link.split('/')[-1]
+        try: mkdir(out+k)
+        except WindowsError: print "\n[FOLDER] %s already exists!\n"%k
+        for i in urls(link):
+            l=urls(i)[0]; m=l.split('/')[-1]
+            if m in listdir(out+k):
+                print "[File] %s already exists!"%m      # path.getsize() can also be used
+                if stat(out+k+'\\'+m).st_size==int(urlopen(l).info()['Content-Length']): continue
+                else:
+                    s=str(raw_input("\tFile has different size! Overwrite (y/n)? "))
+                    if s=='y': print "Downloading",l,"..."; urlretrieve(l,out+k+'\\'+m); continue
+                    else: print "File skipped!"; continue
+            print "Downloading",l,"..."; urlretrieve(l,out+k+'\\'+m)
