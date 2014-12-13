b1f04ad71deedfa5280e65d03f1a9c8a732e2499
Wafflespeanut <wafflescrazypeanut@gmail.com>
2014-12-14 01:52:42 +0530

    Simplified

--- Scrap/Tiny Utilities/Download JPGs.py
+++ Scrap/Tiny Utilities/Download JPGs.py
@@ -1,6 +1,6 @@
 from urllib import *
 from os import mkdir
-import lxml.html
+from lxml.html import *
 from random import choice
 
 o="C:\\Downloads\\"; links=[]
@@ -8,20 +8,19 @@ o="C:\\Downloads\\"; links=[]
 # I used to download comics with this...
 
 def name():     # Just to put random names to the folders!
-    s='c'
-    for i in range(3): s+=str(choice(range(10)))
-    for i in range(3): s+=chr(choice(range(97,123)))
-    return s
+    return 'c'+''.join([str(choice(range(10))) for i in range(3)])+''.join([chr(choice(range(97,123))) for i in range(3)])
+
+def title(link):
+    try: p=parse(urlopen(link)); return p.find(".//title").text
+    except (AttributeError,IOError): return []
 
 def urls(link):
-    dom=lxml.html.fromstring(urlopen(link).read())
-    for l in dom.xpath('//a/@href'):
-        if '#' not in l and 'jpg' in l: return l
+    tree=parse(urlopen(link))
+    return [l for l in tree.xpath('//a/@href') if '#' not in l and 'html' not in l]
 
 def getimg():       # Grabbing a sequential list of 15 images
     for link in links:
         out=o+name()+'//'; mkdir(out)
         for i in range(15):
-            l=link+str(i)+'/'
-            print "Downloading",l,"..."
+            l=link+str(i)+'/'; print "Downloading",l,"..."
             urlretrieve(urls(l),out+str(i+1)+'.jpg')
