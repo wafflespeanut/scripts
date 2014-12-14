d702f5db46234d7e2cba6847d5face2938f39734
Wafflespeanut <wafflescrazypeanut@gmail.com>
2014-12-14 22:40:47 +0530

    Simplified stuff

--- Memento/Diary.py
+++ Memento/Diary.py
@@ -1,10 +1,9 @@
 import os,subprocess
 from time import strftime as time
+from random import choice
 
 loc='C:\\Users\\Waffles Crazy Peanut\\Desktop\\Dropbox\\Diary\\'        # Storage location
 months={'11':'November','10':'October','12':'December','1':'January','3':'March','2':'February','5':'May','4':'April','7':'July','6':'June','9':'September','8':'August'}
-
-# Random stories & viewing stories (with info) are on the way...
 
 def hexed(key):             # Hexing function
     pas=list(key)
@@ -29,21 +28,20 @@ def shift(text,shift):      # Shifts the ASCII value of the chars (reversible)
     return new
 
 def zombify(ch,data,key):
-    k=''.join(hexed(key))
+    k=''.join(hexed(key)); p=data
     if ch=='e':
         p=''.join(hexed(data))
         for i in k: p=shift(p,ord(i)); return p
     elif ch=='d':
-        p=data
         for i in k: p=shift(p,255-ord(i)); return char(p)
 
-def temp(File):
-    protect(File,'d'); subprocess.Popen(["notepad.exe",loc+'TEMP.tmp'])
+def temp(File,key=None):
+    if protect(File,'d',key): subprocess.Popen(["notepad.exe",loc+'TEMP.tmp'])
     #os.remove(loc+'TEMP.tmp')
 
-def protect(path,ch):              # A simple encryption which shifts and turns it to hex!
+def protect(path,ch,key=None):              # A simple encryption which shifts and turns it to hex!
     try:
-        key=raw_input('\nEnter password for your story: ')
+        if key==None: key=raw_input('\nEnter password for your story: ')
         while len(key)<4: key=raw_input('\nEnter password of at least 4 chars: ')
     except KeyboardInterrupt: return False
     with open(path,'r') as file: data=file.readlines()
@@ -57,7 +55,7 @@ def protect(path,ch):              # A simple encryption which shifts and turns 
         with open(path,'w') as file: file.writelines(data)
     elif ch=='d':
         with open(loc+'TEMP.tmp','w') as file: file.writelines(data)
-    return True
+    return key
 
 def write():
     if not os.path.exists(loc+time('%Y')): os.mkdir(loc+time('%Y'))
@@ -65,8 +63,10 @@ def write():
     if not os.path.exists(f): os.mkdir(f)
     File=f+os.sep+'Day '+time('%d')+' ('+months[time('%m')]+' '+time('%Y')+')'
     if os.path.exists(File):
-        print '\nFile already exists! Password required!'
-        while not protect(File,'d'): print 'A password is required to append to an existing file. Running sequence again...'
+        print '\nFile already exists! Password required!'; key=''
+        while not key:
+            key=protect(File,'d')
+            print 'A password is required to append to an existing file. Running sequence again...'
         with open(loc+'TEMP.tmp','r') as file: data=file.readlines()
         with open(File,'w') as file: file.writelines(data)
     os.remove(loc+'TEMP.tmp'); f=open(File,'a')
@@ -77,10 +77,10 @@ def write():
         try: s=raw_input(); a.append(s)
         except KeyboardInterrupt: break
     f.write('\n'.join(a)+'\n\n'); f.close()
-    while not protect(File,'e'):
+    while not protect(File,'e',key):
         print "\nPlease don't interrupt! Your story is insecure! Running sequence again..."
     s=raw_input('\nSuccessfully written to file! Do you wanna see it (y/n)? ')
-    if s=='y': temp(File)
+    if s=='y': temp(File,key)
 
 def view():
     y=raw_input('\nYear: ')
@@ -90,12 +90,24 @@ def view():
         except KeyError: print 'Enter a valid month!'
     if not os.path.exists(loc+y+os.sep+m+' ('+y+')'): print '\nNo stories on this month...'; return None
     d='Day '+raw_input('\nDay: ')+' ('+m+' '+y+')'; f=loc+y+os.sep+m+' ('+y+')'+os.sep+d
-    if not os.path.exists(f): print '\nNo stories this day...'; return None
+    if not os.path.exists(f): print '\nNo stories on this day...'; return None
     else: temp(f)
 
+def random():       # Useful only when you have a lot of stories
+    for i in range(100):        # Just to approach pure randomness instead of pseudo-randomness
+        y=choice(os.listdir(loc)); m=choice(os.listdir(loc+y))
+        d=choice(os.listdir(loc+y+os.sep+m)); f=loc+os.sep+y+os.sep+m+os.sep+d
+    print 'Choosing your story from '+d+'...'; temp(f)
+
 def diary():
-    s=raw_input("\n\tWhat do you wanna do?\n\n\t\t1. Write today's story\n\t\t2. Random story\n\t\t3. View the story of someday\n\nChoice: ")
-    if s=='1': write()
-    elif s=='2': print '\nNot yet implemented - Will be done soon...'
-    elif s=='3': view()
-    else: print '\nIllegal choice! Quitting...'
+    while True:
+        if os.path.exists(loc+'TEMP.tmp'): os.remove(loc+'TEMP.tmp')
+        try:
+            s=raw_input("\n\tWhat do you wanna do?\n\n\t\t1. Write today's story\n\t\t2. Random story\n\t\t3. View the story of someday\n\nChoice: ")
+            if s=='1': write()
+            elif s=='2': random()
+            elif s=='3': view()
+            else: print '\nIllegal choice!'
+            s=raw_input('\nDo something again (y/n)? ')
+            if s!='y': break
+        except KeyboardInterrupt: print '\nQuitting...'; break
