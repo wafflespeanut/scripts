f6d8929f4c1a69431261036a6b4cc2aab264d523
Wafflespeanut <wafflescrazypeanut@gmail.com>
2014-12-21 21:58:24 +0530

    I love try-catch

--- Scrap/Tiny Utilities/Rename Files.py
+++ Scrap/Tiny Utilities/Rename Files.py
@@ -2,25 +2,25 @@ import os,subprocess,eyed3,shutil
 
 path="C:\\TEMP\\"
 
-def renvid():           # To rename video files after watching
+def ren():           # To rename video files after watching
     for i in os.listdir(path):
-        p=subprocess.Popen(["vlc.exe",path+i])      # Meh, I could've used os.startfile()
+        p=subprocess.Popen(["vlc.exe",path+i])      # os.startfile() could be used for opening all files!
         print "\nOld name:",i; s=str(raw_input('New name: '))
         if s=='': continue
         elif s=='!': break
-        try: os.rename(path+i,path+s)
-        except WindowsError:
-            raw_input('\n\tFile being used! Continue after closing...')
-            os.rename(path+i,path+s)
+        while True:
+            try: os.rename(path+i,path+s); break
+            except WindowsError:
+                raw_input('\n\tFile being used! Continue after closing...'); continue
 
-def renum():            # To rename numbered ones
+def num():            # To rename numbered ones
     for i in os.listdir(path):
         k=0
         if i[0] in '1234567890':
             while i[k] in '1234567890' or i[k] in ' -': k+=1
         os.rename(path+i,path+i[k:])
 
-def renmp3():           # To rename MP3s based on their metadata
+def mp3():           # To rename MP3s based on their metadata
     for i in os.listdir(path):
         f=eyed3.load(path+i)
         os.rename(path+i,path+f.tag.title+'.mp3')
