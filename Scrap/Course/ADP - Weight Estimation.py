0d3cdb81c2c60b65a0aeee2dc937d545bb6e73c1
Wafflespeanut <wafflescrazypeanut@gmail.com>
2015-02-02 19:23:02 +0530

    Reversing Iterations...

--- Scrap/Course/ADP - Weight Estimation.py
+++ Scrap/Course/ADP - Weight Estimation.py
@@ -1,19 +1,49 @@
 execfile("Standard Atmosphere.py")
 
-def find(w0=275300,we=130199,tl=0.2659,ca=10448,cv=0.83505,wp=51720,wc=2240,r=7000,n=4):
-    cv*=((1.4*287*atmos(ca)[0])**0.5)*(18.0/5); wf=0; rf=0.3; wpp=0; d0=1.225; i=0; re=re0=float(we)/w0
+def fan(w0=275300,we=130199,tl=0.2659,ca=10448,cv=0.83505,wp=51720,wc=2240,r=7000,n=4):
+    a=[]; cv*=((1.4*287*atmos(ca)[0])**0.5)*(18.0/5); wf=0; rf=0.3; wpp=0; d0=1.225; i=0; re=re0=float(we)/w0
+    while True:
+        try:
+            a.append([w0,we,wpp,wf,re,rf])
+            print '\nIteration %s'%(i)
+            w0=(wc+wp+wpp+wf)/(1-re-rf)
+            print '\nNet weight: %s kg'%(w0)
+            t=w0*tl*9.81*1.2
+            print 'Thrust (per engine): %s lbf'%(t/(4.448*n))
+            wpp=float(raw_input('Engine Weight (lb): '))*0.4535924*n
+            te=float(raw_input('Engine Thrust (lbf): '))*4.448*n
+            tc=te*(atmos(ca)[2]/d0)**1.2
+            print 'Thrust at Cruise (per engine): %s N'%(tc/n)
+            sfc=float(raw_input('SFC (lb/lbf-hr): '))
+            wf=(sfc*tc*r*1.2)/(cv*9.81); rf=0; re=re0-wpp/w0
+            print 'Fuel Weight ratio: %s'%(wf/w0)
+            print 'Empty Weight ratio: %s'%(re)
+            raw_input('\nPress [Enter] to continue...'); i+=1
+        except KeyboardInterrupt:
+            if i==0:
+                print '\n\tThis is the first iteration!'
+                w0,we,wpp,wf,re,rf = (z for z in a[i]); del a[-1]; continue
+            try:
+                w0,we,wpp,wf,re,rf = (z for z in a[i]); del a[-1]
+                raw_input('\n\t(Resetting values!) Press [Enter] to continue...')
+            except KeyboardInterrupt:
+                i-=1; w0,we,wpp,wf,re,rf = (z for z in a[i]); del a[-1]
+                print '\n\t(Getting back to previous iteration...)\n'
+
+def prop(w0=14333,we=7400,pl=0.294906,ca=8000,cv=500,wp=3600,wc=400,r=1800,n=2):
+    wf=0; rf=0.15; wpp=0; d0=1.225; i=0; re=re0=float(we)/w0
     while True:
         print '\nIteration %s'%(i)
         w0=(wc+wp+wpp+wf)/(1-re-rf)
         print '\nNet weight: %s kg'%(w0)
-        t=w0*tl*9.81*1.2
-        print 'Thrust (per engine): %s lbf'%(t/(4.448*n))
+        p=w0*pl*1.2
+        print 'Power (per engine): %s shp'%(p/n)
         wpp=float(raw_input('Engine Weight (lb): '))*0.4535924*n
-        te=float(raw_input('Engine Thrust (lbf): '))*4.448*n
-        tc=te*(atmos(ca)[2]/d0)**1.2
-        print 'Thrust at Cruise (per engine): %s N'%(tc/n)
+        pe=float(raw_input('Engine Power (shp): '))*n
+        pc=pe*(atmos(ca)[2]/d0)**1.2
+        print 'Cruise Power (per engine): %s shp'%(pc/n)
         sfc=float(raw_input('SFC (lb/lbf-hr): '))
-        wf=(sfc*tc*r*1.2)/(cv*9.81); rf=0; re=re0-wpp/w0
+        wf=(sfc*pc*r*1.2*0.4535924)/(cv); rf=0; re=re0-wpp/w0
         print 'Fuel Weight ratio: %s'%(wf/w0)
         print 'Empty Weight ratio: %s'%(re)
         raw_input('\nPress [Enter] to continue...'); i+=1
