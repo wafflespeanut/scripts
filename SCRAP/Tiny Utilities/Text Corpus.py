15dd071def024ebb6b7a45422ee687f34c6ff7d1
Wafflespeanut <wafflescrazypeanut@gmail.com>
2015-03-30 01:28:00 +0530

    One hell of update!

--- SCRAP/Tiny Utilities/Text Corpus.py
+++ SCRAP/Tiny Utilities/Text Corpus.py
@@ -4,9 +4,16 @@ from random import shuffle
 
 desk=path.expanduser('~\Desktop'); out=desk+'\\TEMP\\Text Corpus\\'
 link='http://www.gutenberg.org/browse/scores/top'
+export=desk+'\\Github\\Python\\SCRAP\\FREQ.txt'
 
 # Downloads the first 20 books (as text files) from the Project Gutenberg homepage
 # They don't want us to download a lot of stuff in a single day (Well, they block our IPs soon). So, let's limit it to 20...
+
+def read(f):
+    with fopen(f,'r') as file: return file.readlines()
+
+def write(loc,s):
+    with fopen(loc,'w') as file: file.writelines(s)
 
 def booklist():             # Generates a list of books
     s='http://www.gutenberg.org/cache/epub/'; blist=[]
@@ -21,7 +28,7 @@ def get():                  # Downloads the books
 def revise():               # A rough cleanup of Gutenberg's license & junks...
     for f in listdir(out):
         if 'pg' in f:
-            with fopen(out+f,'r') as file: data=file.readlines()
+            data=read(out+f)
             for i,j in enumerate(data[::-1]):
                 if ("End of" in j and "Project Gutenberg" in j): break
             if (len(data)-i-1)==0:
@@ -30,38 +37,38 @@ def revise():               # A rough cleanup of Gutenberg's license & junks...
             data=data[:len(data)-i-1]; i=0; j=0
             while 'Produced' not in data[i] and i<30: i+=1
             while i==30 and '***' not in data[j] and j<30: j+=1
-            with fopen(out+f,'w') as file: file.writelines(''.join(data[min(i,j)+4:]))
+            write(out+f,''.join(data[min(i,j)+4:]))
             rename(out+f,out+f[2:])
 
 # Cleanup also requires a checkup on hand to ensure that we don't have garbage stuff
 
 def scan(rmin=32,rmax=127):                # ASCII letter frequencies in range(32,127)
-    freq=[0 for i in range(rmax)]; c=0
+    freq=[0 for i in range(rmax)]; c=0; z=0
     def cleandict(ls):                      # Converts to dict and removes unnecessary keys
         dic=dict(enumerate(ls))
         for k in range(len(dic)):
             if not dic[k]: del dic[k]
         return dic
     for f in listdir(out):
-        if '.txt' not in f: continue
-        temp=[0 for i in range(rmax)]; ct=0
-        with fopen(out+f,'r') as file: data=file.readlines()
+        if f=='STATS-MAIN.txt' or '.txt' not in f: continue
+        print 'Scanning %s...'%(f); z+=1
+        temp=[0 for i in range(rmax)]; ct=0; data=read(out+f)
         for i in ''.join(data):
             m=ord(i)
             if m>=rmin and m<rmax: temp[m]+=1; c+=1; ct+=1
         freq=[freq[i]+temp[i] for i in range(rmax)]; dtemp=cleandict(temp)
         #if not path.exists(out+'STATS'): mkdir(out+'STATS')
         #stats((dtemp,ct),'STATS\\stat-'+f)         # Use this for statistics from individual files
-    print 'Scanned a total of %d characters (in the given range) from %d files!\n'%(c,len(listdir(out)))
+    print 'Scanned a total of %d characters (in the given range) from %d files!\n'%(c,z)
     dfreq=cleandict(freq)
     return (dfreq,c)
 
 def stats(stuff=None,F='STATS-MAIN.txt'):
     if path.exists(out+F):
-        s=raw_input('\nSTATS file already exists! Continue? (y/n)')
+        s=raw_input('\nSTATS file already exists! Continue? (y/n): ')
         if s!='y': startfile(out+F); return
     if not stuff: stuff=scan()
-    w=[]; dfreq=stuff[0]; c=stuff[1]; tab=''
+    w=[]; dfreq=stuff[0]; c=stuff[1]; tab=''; z=[]
     sc='Scanned a total of %d characters! (in the given range)\n'%(c)
     w.append('\nASCII Character Frequencies: %s'%(sc))
     w.append("Most frequently found character: '%s'"%(chr(max(dfreq.iterkeys(),key=(lambda key: dfreq[key])))))
@@ -72,5 +79,6 @@ def stats(stuff=None,F='STATS-MAIN.txt'):
         if len(str(dfreq[i]))>=4: tab='\t\t\t'
         else: tab='\t\t\t\t'
         w.append('%d\t\t%s\t\t%d%s%.10f %s'%(i,chr(i),dfreq[i],tab,dfreq[i]*100/float(c),chr(ord('%'))))
-    with fopen(out+F,'w') as file: file.writelines('\n'.join(w))
+        z.append('%d\t%.10f'%(i,dfreq[i]/float(c)))
+    write(out+F,'\n'.join(w)); write(export,'\n'.join(z))
     startfile(out+F)
--- SCRAP/Variant Vignere.py
+++ SCRAP/Variant Vignere.py
@@ -1,20 +1,49 @@
 from math import ceil
+from os import path
 
-fin="CTEXT.txt"; fout="CTEXT.txt"
+fin="CTEXT.txt"; fout="YAY.txt"
+freq='FREQ.txt' # Generated using data from a known text corpus
+key=[] # Key in hex byte strings
 
-# Known letter frequencies
-fre = {97: 8.2, 98: 1.5, 99: 2.8, 100: 4.3, 101: 12.7, 102: 2.2, 103: 2.0, 104: 6.1, 105: 7.0, 106: 0.2, 107: 0.8, 108: 4.0, 109: 2.4, 110: 6.7, 111: 1.5, 112: 1.9, 113: 0.1, 114: 6.0, 115: 6.3, 116: 9.1, 117: 2.8, 118: 1.0, 119: 2.4, 120: 0.2, 121: 2.0, 122: 0.1}
+def read(f=fin):
+    with open(f,'r') as file: return file.readlines()
+
+def write(s,f=fout):
+    with open(f,'w') as file: file.writelines(s)
 
 def hexor(ch,k): return ord(ch.decode('hex'))^ord(k.decode('hex'))
 
 # It deviates from the usual Vigenere in that it XORs hexed plaintext & key
 
+def IC(ls):          # Find the index of coincidences
+    data=read(); ctext=[data[0][i:i+2] for i in range(0,len(data[0]),2)]; avg=0
+    for k in range(ls):
+        s=[ctext[i] for i in range(k,len(ctext),ls)]; d={}
+        for i in set(ctext): d[i]=s.count(i)
+        ic=sum([d[i]*(d[i]-1) for i in d])/(float(len(s))*(len(s)-1))
+        print 'Sequence %d (length=%d): %f'%(k+1,len(s),ic); avg+=ic
+    return avg/ls
+
+def analyze(rmin=2,rmax=13):        # Find the ICs for a range of keyspaces
+    d=[0,0]
+    for k in range(rmin,rmax):
+        print '\nAssuming keyspace=%d...\n'%(k)
+        avg=IC(k); print '\n\tAverage IC: %f'%(avg)
+        if avg>d[1]: d[1]=avg; d[0]=k
+    print '\nHighest IC achieved: %f (for keyspace=%d)'%(d[1],d[0])
+    return d[0]
+
+def snoop():
+    ksp=7; fread=read(freq); d=dict(enumerate([0 for i in range(128)]))
+    for i in fread: m=i.split('\t'); d[int(m[0])]=float(m[1][:-1])
+    return dfreq
+
 def cipher(ch):
-    with open(fin,'r') as file: data=file.readlines()
+    data=read(); k=[]
     if ch=='e': ptext=[format(ord(i),'02x') for i in data[0]]
     elif ch=='d': ptext=[data[0][i:i+2] for i in range(0,len(data[0]),2)]
     else: print 'Invalid option!'; return None
-    k=[]; [k.extend(key) for i in range(int(ceil(len(ptext)/float(len(key)))))]
+    [k.extend(key) for i in range(int(ceil(len(ptext)/float(len(key)))))]
     if ch=='e': s=[format(hexor(ptext[i],k[i]),'02x') for i in range(len(ptext))]
     elif ch=='d': s=[chr(hexor(ptext[i],k[i])) for i in range(len(ptext))]
-    with open(fout,'w') as file: file.writelines(''.join(s))
+    print ''.join(s); #write(''.join(s))
