38cec173ea8947af6761fa3bf94c63ac04da58a4
Wafflespeanut <wafflescrazypeanut@gmail.com>
2015-03-29 20:17:05 +0530

    Updated...

--- SCRAP/Tiny Utilities/Text Corpus.py
+++ SCRAP/Tiny Utilities/Text Corpus.py
@@ -11,17 +11,29 @@ link='http://www.gutenberg.org/browse/scores/top'
 def booklist():
     s='http://www.gutenberg.org/cache/epub/'; blist=[]
     for i in set(urls(link,'ebooks')[1:]): m=i.split('/')[-1]; blist.append(s+m+'/pg'+m+'.txt')
-    return shuffle(blist)[:20]
+    shuffle(blist); return blist
 
 def get():
-    for l in booklist(): print 'Downloading',l,'...'; urlretrieve(l,out+l.split('/')[-1])
+    for l in booklist():
+        if l.split('/')[-1] not in listdir(out) and l.split('/')[-1][2:] not in listdir(out):
+            print 'Downloading',l,'...'; urlretrieve(l,out+l.split('/')[-1])
 
-def revise():               # A rough cleanup to Gutenberg's license & junks...
+def revise():               # A rough cleanup of Gutenberg's license & junks...
     for f in listdir(out):
-        with fopen(out+f,'r') as file: data=file.readlines()
-        i=0; data=data[:-362]
-        while '***' not in data[i]: i+=1
-        with fopen(out+f,'w') as file: file.writelines(''.join(data[i+2:]))
+        if 'pg' in f:
+            with fopen(out+f,'r') as file: data=file.readlines()
+            for i,j in enumerate(data[::-1]):
+                if ("End of" in j and "Project Gutenberg" in j): break
+            if (len(data)-i-1)==0:
+                for i,j in enumerate(data[::-1]):
+                    if ("END OF" in j and "PROJECT GUTENBERG" in j): break
+            data=data[:len(data)-i-1]; i=0; j=0
+            while 'Produced' not in data[i] and i<30: i+=1
+            while i==30 and '***' not in data[j] and j<30: j+=1
+            with fopen(out+f,'w') as file: file.writelines(''.join(data[min(i,j)+4:]))
+            rename(out+f,out+f[2:])
+
+# Cleanup also requires a checkup on hand to ensure that we don't have garbage stuff
 
 def scan(rmin=32,rmax=127):                # ASCII letter frequencies in range(32,127)
     freq=[0 for i in range(0,rmax)]; c=0
@@ -30,7 +42,7 @@ def scan(rmin=32,rmax=127):                # ASCII letter frequencies in range(3
         for i in ''.join(data):
             m=ord(i)
             if m>=rmin and m<rmax: freq[m]+=1; c+=1
-    print 'Scanned a total of %d characters from %d files!\n'%(c,len(listdir(out)))
+    print 'Scanned a total of %d characters (in the given range) from %d files!\n'%(c,len(listdir(out)))
     dfreq=dict(enumerate([float(i)/c for i in freq]))
     for k in range(len(dfreq)):
         if not dfreq[k]: del dfreq[k]
