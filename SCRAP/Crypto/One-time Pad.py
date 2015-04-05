5fe91645c082f31b6ccf16dcb3031ad9f799ecfb
Wafflespeanut <wafflescrazypeanut@gmail.com>
2015-04-05 15:17:20 +0530

    Landed on a boring algo!

--- SCRAP/Crypto/One-time Pad.py
+++ SCRAP/Crypto/One-time Pad.py
@@ -2,32 +2,51 @@ execfile("Variant Vigenere.py")
 from math import ceil
 from os import path
 from time import sleep
-from itertools import combinations as comb
 
-fin="CTEXT-2.txt"; key=[] # Key in hex byte strings
+fin="CTEXT-2.txt"; # Key in hex byte strings
+key = ['00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00', '00']
+# That's how you start!
 
-def xor(c1,c2):
+key = ['F2', '1A', '04', '9B', 'D0', '73', '23', 'C8', '39', '98', 'CE', '09', '0E', 'BC', '86', 'DA', 'C9', 'E0', '39', '89', '2A', '5F', '72', '67', '83', 'A5', '61', 'FD', '25', 'EE', '30']
+# Key obtained by repetitive ctext(n) and test() - Boring!
+
+def check(c1,c2):
     if not len(c1)==len(c2): return None
     t1=[c1[i:i+2] for i in range(0,len(c1),2)]; t3=[]
     t2=[c2[i:i+2] for i in range(0,len(c2),2)]; t4=[]
     for i in range(len(t1)):
         p=str(hexor(t1[i],t2[i]))
         s=(' '+p if len(p)<2 else p); t3.append(s)
-        t4.append(' '+chr(32^int(s)) if int(s)>40 else '  ')
-    print '  '.join(t1),'\n','  '.join(t2),'\n','  '.join(t4)
-##    for i in range(0,len(c1),2):
-##        m=hexor(c1[i:i+2],c2[i:i+2]); x=(8-len(bin(m)[2:]))*'0'+bin(m)[2:]
-##        s=('Space | Char' if x[:2]=='00' else 'Space & Char'); ch=''; k=''
-##        if x=='0'*8: s='Same char!'
-##        elif x[:2]=='01': ch=chr(m^32); k=i/2
-##        print c1[i:i+2],'\t',c2[i:i+2],'\t',m,'\t',x,'\t',s,'\t',ch,'\t',k
-    print '\n'; return None
+        t4.append(' '+chr(32^int(s)) if 32^int(s) in range(65,122) else '  ')
+    return (t1,t2,t3,t4)
 
-def ctexts():
+def ctext(m):           # Rough way to guess the keys
+    with open(fin,'r') as file: data=file.readlines()
+    data=[i[:-1] if i[-1]=='\n' else i for i in data]; k=key
+    r=(' '+str(i) if i/10==0 else str(i) for i in range(len(data[m-1])/2))
+    print '  '.join(r),'\n'
+    for i in range(len(data)):
+        (t1,t2,t3,t4)=check(data[m-1],data[i])
+        if m==(i+1): continue
+        print '  '.join(t4),' ',(m,i+1)
+    collect=t1
+    while True:
+        try:
+            p=int(raw_input('\nPosition: '))
+            ch=raw_input('Possible character: ')
+            try: ch=format(ord(ch),'02x')
+            except TypeError: ch=format(ord(ch.split(' ')[-1]),'02x')
+            l=collect[p]; k[p]=format(hexor(l,ch),'02x').upper()
+        except (KeyboardInterrupt,ValueError): print 'Testing new key...\n'; test(k); print; return k
+
+def test(k=key):
     with open(fin,'r') as file: data=file.readlines()
     data=[i[:-1] if i[-1]=='\n' else i for i in data]
-    return (list(comb(data,2)),list(comb(range(len(data)),2)))
+    r=(' '+str(i) if i/10==0 else str(i) for i in range(len(k))); print '  '.join(r),'\n'
+    for b,c in enumerate(data):
+        s=[]; ch=[c[i:i+2] for i in range(0,len(c),2)]
+        for i in range(len(ch)):
+            m=hexor(ch[i],k[i]); s.append(' '+chr(m))
+        print '  '.join(s),'\t',[b+1]
 
-c=ctexts()[1]
-for i,j in enumerate(ctexts()[0]):
-    print 'M%s && M%s'%c[i]; xor(j[0],j[1])
+test()
