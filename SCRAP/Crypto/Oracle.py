47694bb2235040354e3d22e21e5d4e11ec0fdd02
Wafflespeanut <wafflescrazypeanut@gmail.com>
2015-04-19 21:55:02 +0530

    Almost done!

--- SCRAP/Crypto/Oracle.py
+++ SCRAP/Crypto/Oracle.py
@@ -1,24 +1,22 @@
 s=None
-# For a server which returns [0,1] depending on how we modify the ciphertexts
+# For a server which returns (0,1) depending on how we modify the ciphertexts
 
 def connect():
     import socket; global s
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     try: s.connect(('54.165.60.84',80))
     except socket.error as e: print e; return -1
-    print "Connected to server successfully..."
-    return 0
+    print "Connected to server!"; return 0
 
 def disconnect():
-    if not s:
-        print "[WARNING]: You haven't connected to the server yet!"; return -1
-    s.close(); print "Connection closed successfully..."; return 0
+    if not s: 'Not connected!'; return 0
+    s.close(); print "Connection closed!"; return 0
 
 # Packet Structure: < num_blocks(1) || ciphertext(16*num_blocks) || null-terminator(1) >
 
-def send(ctext,num_blocks):
-    if not s: print "[WARNING]: You haven't connected to the server yet!"; return -1
-    msg = ctext[:]; msg.insert(0,num_blocks); msg.append(0)
-    s.send(bytearray(msg)); recvbit = s.recv(2)
+def send(ctext,blocks):
+    if not s: return -1
+    msg=ctext[:]; msg.insert(0,blocks); msg.append(0)
+    s.send(bytearray(msg)); recvbit=s.recv(2)
     try: return int(recvbit)
     except ValueError as e: return int(recvbit[0])
---
+++ SCRAP/Crypto/Padding.py
@@ -0,0 +1,56 @@
+execfile("Oracle.py")
+import os,sys
+from time import sleep
+
+# A server knows the key. Our job is to find the key by sending & receiving packets...
+
+def hexor(c1,c2): return int(c1,16)^int(c2,16)
+
+def hexed(p): return ''.join([format(i,'02x').upper() for i in p])
+
+def read(File,blocks):      # Our ciphertext had 3 blocks appended as IV|C1|C2
+    with open(File,'r') as file: data=file.readlines()
+    s=data[0]; c=len(s)/blocks
+    return [s[i:i+c] for i in range(0,len(s),c)]
+
+def test(data,blocks):   # Usage: `python Padding.py <filename>`
+    ctext=[(int(data[i:i+2],16)) for i in range(0,len(data),2)]
+    print "\nCTEXT:",ctext,'\n\nSending ciphertext...'
+    try: rc=send(ctext,blocks)
+    except IndexError: return None
+    print "Oracle returned: %d\n" %(rc); return rc
+
+def analyze(File='CBC.txt',b=3):
+    data=read(File); co=connect()
+    if co==-1: 'Connection failed!'; return
+    rc=test(''.join(data),b); p=data[b-2]
+    print 'Oracle responds!'
+    p=[p[i:i+2] for i in range(0,len(p),2)]
+    print 'Chosen block:',p; i=0
+    while True:    # nth block is padded using PKCS #5, and so modify (n-1)th block to find key length!
+        p[i]='00'; data[b-2]=''.join(p)
+        print 'Modified block:',p,'\nPosition:',i+1
+        if not test(''.join(data),b): break
+        sleep(1); i+=1
+    disconnect(); print 'Last %d bytes are padded!'%(len(p)-i)
+    return i
+
+def find(File='CBC.txt',b=3):
+    l=5; data=read(File,b); k=data[b-2]; co=connect(); a={}
+    if co==-1: print 'Connection failed!'; return
+    for j in range(l):
+        p=len(k)/2-l+j; print 'Padding key: %s (%s)'%(p,format(p,'02x').upper())
+        k=[int(k[i:i+2],16) if i<2*(l-j) else int(k[i:i+2],16)^p^(p+1) for i in range(0,len(k),2)]
+        i=l-(j+1); t=k[i]; k[i]=0; sleep(5)
+        while True:
+            data[b-2]=hexed(k); print 'Modified block:',data[b-2]
+            rc=test(''.join(data),b)
+            if rc==None: connect(); print 'Retrying...'; continue
+            k[i]+=1
+            if rc==1: k[i]-=1; a[i]=p^(t^k[i]); k=hexed(k); break
+    disconnect(); return k
+
+##if len(sys.argv)>=2: test(sys.argv[1])
+##else: test()
+
+find()
