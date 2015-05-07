a154591a7e46784c1cef307c912e752fb0d45878
Wafflespeanut <wafflespeanut@gmail.com>
2015-05-07 17:29:38 +0530

    Okay'ish...

--- SCRAP/Django/Github-Repos/repos/views.py
+++ SCRAP/Django/Github-Repos/repos/views.py
@@ -5,7 +5,8 @@ def repo_list(user):
     page=urllib.urlopen("https://api.github.com/users/"+user+'/repos')
     stuff=page.read().replace('false','False').replace('true','True').replace('null','None')
     d=ast.literal_eval(stuff)
-    if not d: return None
+    if not d: return False
+    elif 'message' in d: return None
     return [i['name'] for i in d]
 
 def search_form(request):
@@ -13,19 +14,22 @@ def search_form(request):
     return HttpResponse(''.join(data))
 
 def search(request):
-    if not len(request.GET['q']): return HttpResponse('Empty search query!')
-    user=request.GET['q']; repo=repo_list(user)
-    if repo==None: return HttpResponse("User doesn't exists!")
-    l=['https://github.com/%s/%s'%(user,repo[i]) for i in range(len(repo))]
-    m=['<tr><td>%s.</td><td><a href="%s">%s</a></td></tr>'%(i+1,l[i],j) for i,j in enumerate(repo)]
-    s='<table>%s</table>'%'\n'.join(m)
+    if not len(request.GET['q']): s='Empty search query!'
+    else: user=request.GET['q']; repo=repo_list(user)
+    if repo==False: s="<h2>User doesn't have any repositories (yet)!</h2>"
+    elif repo==None: s="<h2>User doesn't exists!</h2>"
+    else:
+        link=['https://github.com/%s/%s'%(user,repo[i]) for i in range(len(repo))]
+        m=['<tr><td>%s.</td><td><a href="%s">%s</a></td></tr>'%(i+1,link[i],j) for i,j in enumerate(repo)]
+        s='''<p style="padding:20px;">(%s has contributed to %d repositories...)</p>
+                <table style="padding:50px;"> %s </table>'''%(user,len(m),'\n'.join(m))
     stuff='''
         <!DOCTYPE html>
         <html>
-            <head><title>Search results</title></head>
-            <body>%s
-            <div align=center>%s</div>
+            <head><title>Search results...</title></head>
+            <body> %s
+            %s
             </body>
         </html>
-        '''%('<br>'*5,s)
+        '''%('<br>'*3,s)
     return HttpResponse(stuff)
