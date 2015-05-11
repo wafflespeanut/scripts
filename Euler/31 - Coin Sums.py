aa3cdbd2c3a0c974b0adbf2bdeba3eb1c647d8af
Wafflespeanut <wafflescrazypeanut@gmail.com>
2015-05-11 21:22:27 +0530

    Back on track...

--- Euler/31 - Coin Sums.py
+++ Euler/31 - Coin Sums.py
@@ -1,8 +1,8 @@
-def ways(n):
-    coins=[1,2,5,10,20,50,100,200]
+def ways(n,stuff):
     ways=[1]+[0 for i in range(n)]
-    for i in coins:
+    for i in stuff:
         for j in range(i,n+1): ways[j]+=ways[j-i]
     return ways[n]
 
-#n=200; print "There are " +str(ways(n))+ " ways to get to " +str(n)+ " pennies!"
+#coins=[1,2,5,10,20,50,100,200]
+#n=200; print "There are " +str(ways(n,coins))+ " ways to get to " +str(n)+ " pennies!"
---
+++ Euler/76 - Counting Summations.py
@@ -0,0 +1,4 @@
+execfile("31 - Coin Sums.py")
+
+#n=100; num=range(1,n)
+#print 'There are',ways(n,num),'to write',n,'as a sum of at least two positive integers!'
