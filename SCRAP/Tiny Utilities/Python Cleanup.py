a8b63a457a22fab0eb4cd18aa1c47f252c03a902
Wafflespeanut <wafflescrazypeanut@gmail.com>
2015-05-26 21:29:05 +0530

    It's always buggy (sigh)

--- SCRAP/Tiny Utilities/Python Cleanup.py
+++ SCRAP/Tiny Utilities/Python Cleanup.py
@@ -9,18 +9,28 @@ def search(path=path,ext='py'):             # For listing all those .py files
         fileList.extend(temp)
     return fileList
 
-def symbols(line):        
-    punc=['"',"'"]; i=0; copy=list(line); math='+-/*=%<>^!|&'
+def symbols(line):
+    if line=='\n': return '\n'
+    punc=['"',"'"]; i=0; copy=list(line); sym='+-/*=%<>^!|&'
     ch=''.join(line.split())[0]; tab=line.index(ch)
     
     def string(index):                  # Skip strings
-        copy=list(line); ch=copy[index]; i=index+1
+        ch=copy[index]; i=index+1
+        if line[index:index+3]=="'''": return line.index("'''")
         while copy[i] is not ch:
-            if copy[i]=='\\' and copy[i+1]==ch: i+=1
+            if copy[i]=='\\': i+=1
             i+=1
         return i
 
+    def math(stuff,index):              # Spaced symbols
+        i=index; copy=stuff[:]
+        if copy[i-1] is not ' ': copy[i]=' '+copy[i]
+        if copy[i+1] is '=' and copy[i+2] is not ' ': copy[i+1]='= '; i+=1
+        elif copy[i+1] is not ' ': copy[i]+=' '
+        return copy,i
+
     while i<len(copy):
+        if copy[i] in ['\n','#']: break
         if copy[i] in punc: i=string(i)
         elif copy[i]==',' and copy[i+1] is not ' ': copy[i]+=' '
         elif copy[i]=='{':              # Grouped dicts
@@ -28,24 +38,25 @@ def symbols(line):
             while True:
                 i+=1
                 if copy[i] in punc: i=string(i)
-                if copy[i] is ',': copy[i]+='\n'+sp*' '
+                elif copy[i] in sym: copy,i = math(copy,i)
+                elif copy[i] is ',': copy[i]+='\n'+sp*' '
                 elif copy[i] is ':' and copy[i+1] is not ' ': copy[i]+=' '
                 elif copy[i] is '}':
                     copy[i]='\n'+tab*' '+'}'; break
         elif copy[i]=='[':              # Spaced lists
-            i+=1; items=''
+            items=''
             while copy[i] is not ']':
+                i+=1
                 if copy[i] in punc: i=string(i)
-                items+=copy[i]; i+=1
-            if ',' in items: i-=len(items); continue
+                elif copy[i] in sym: copy,i = math(copy,i)
+                elif copy[i]==',' and copy[i+1] is not ' ': copy[i]+=' '
+                items+=copy[i]
         elif copy[i]==':':              # Colons with newlines
-            tab+=4
+            if copy[i+1]=='\n': i+=1; continue
+            else: tab+=4
             if copy[i+1]==' ': copy[i+1]='\n'+tab*' '
             elif copy[i+1] is not '\n': copy[i]=':\n'+tab*' '
-        elif copy[i] in math:           # Spaced symbols
-            if copy[i-1] is not ' ': copy[i]=' '+copy[i]
-            if copy[i+1] is '=' and copy[i+2] is not ' ': copy[i+1]='= '; i+=1
-            elif copy[i+1] is not ' ': copy[i]+=' '
+        elif copy[i] in sym: copy,i = math(copy,i)
         elif copy[i]==';':              # Abandon semicolons!
             copy[i]='\n'+tab*' '; i+=1
             while copy[i]==' ': copy[i]=''; i+=1
@@ -55,6 +66,9 @@ def symbols(line):
 def cleanup(File):
     with open(File,'r') as file: data=file.readlines()
     return data
+    from time import sleep
+    for i in data:
+        print symbols(i); sleep(1)
 
 a=cleanup(path)             # Only for testing purposes...
 while True:
