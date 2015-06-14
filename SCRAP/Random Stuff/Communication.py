b29934d9a5cc129be126bc1908bf28d245594527
Ravi Shankar <wafflespeanut@gmail.com>
2015-06-15 02:18:35 +0530

    Much better now...

--- SCRAP/Random Stuff/Communication.py
+++ SCRAP/Random Stuff/Communication.py
@@ -1,7 +1,7 @@
 import random
 nameList = []
 
-def calls(people = nameList):           # This is still an exhaustive way
+def calls(people = nameList):           # This is still an exhaustive way - goes as O(n^2)
     num = len(people)
     assigned = dict(zip(people,[[] for i in people]))
     noluck = False
@@ -12,16 +12,27 @@ def calls(people = nameList):           # This is still an exhaustive way
     print total, 'calls have to be made! Each user has to make at least', lowerLimit, 'calls!'
     if total % num:
         noluck = total % num            # Notice that also, noluck = len(people) / 2
-        print '\nUnequalized calls!', noluck, 'unlucky users who gotta make an extra call!'
+        print 'Unequalized calls!', noluck, 'unlucky users gotta make an extra call!'
     else:
-        print '\nBalanced calls...'
-    for person in people:
+        print 'Balanced calls...'
+    random.shuffle(people)
+    for person1 in people:
         count = 0
-        while count < lowerLimit:
-            choice = random.choice(range(0,len(people)))
-            if people[choice] is not assigned[person]:
-                if people[choice] not in assigned[person]:
-                    if person not in assigned[people[choice]]:
-                        assigned[person].append(people[choice])
-                        count += 1
-    return assigned
+        for person2 in people:
+            if all([person1 is not person2, person1 not in assigned[person2], person2 not in assigned[person1]]):
+                assigned[person1].append(person2)
+                count += 1
+            if count == lowerLimit:
+                break
+    if noluck:
+        unlucky = []
+        for person1 in people:
+            for person2 in people:
+                if all([person1 is not person2, person2 not in assigned[person1], person1 not in assigned[person2]]):
+                    assigned[person1].append(person2)
+                    unlucky.append(person1)
+                if len(unlucky) == noluck:
+                    break
+        print '\nUnlucky users:', unlucky, '\n'
+    for person in assigned:
+        print '%s: %s' % ([person], ', '.join([str(i) for i in assigned[person]]))
