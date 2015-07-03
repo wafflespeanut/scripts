ea3dd4f890891972f707ddd5d74d04145fdb48b4
Ravi Shankar <wafflespeanut@gmail.com>
2015-07-03 20:50:55 +0530

    Phew, it works...

--- rust/finite-diff/src/main.rs
+++ rust/finite-diff/src/main.rs
@@ -1,23 +1,22 @@
 
-fn check(matrix: &Vec<f32>, rows: usize, cols: usize, tol: f32) {
-    let (mut i, mut j) = (cols + 1, 1);
-    let (mut now, mut then) = (matrix.clone(), matrix.clone());
-    let mut count = 0;
-    while count < (rows - 2) * (cols - 2) {
-        then = now.clone();
-        now[i] = (then[i - 1] + then[i + 1] + then[i - cols] + then[i + cols]) / 4.0;
-        if (now[i] - then[i]) <= tol { count += 1; }
-        println!("{:?}", (&now, &then, i, j));
-        if i == j * cols - 2 {
-            if j == rows - 2 {
-                i = cols + 1; j = 1;
-                count = 0; continue
+fn iterate(matrix: &Vec<f64>, rows: usize, cols: usize, toler: f64) -> Vec<f64> {
+    let (mut i, mut j) = (cols + 1, 2);
+    let (mut new, mut old) = (matrix.clone(), matrix.clone());
+    let (mut num_agreed, mut num_iter) = (0, 0);
+    while num_agreed < (rows - 2) * (cols - 2) {
+        if i > j * cols - 2 {
+            if j >= rows - 2 {
+                i = cols + 1; j = 2;
+                num_agreed = 0; num_iter += 1; continue
             } else { i += 3; j += 1; continue }
-        } i += 1;
-    }
+        } old = new.clone();
+        new[i] = (old[i - 1] + old[i + 1] + old[i - cols] + old[i + cols]) / 4.0;
+        if (new[i] - old[i]).abs() <= toler { num_agreed += 1; }
+        i += 1;
+    } println!("\nNumber of iterations: {}\n", num_iter); new
 }
 
 fn main() {
-    let stuff: Vec<f32> = vec![2.0, 3.0, 5.0, 2.0, 4.5, 2.0, 0.0, 0.0, 0.0, 5.0, 6.0, 4.0, 3.0, 5.5, 7.0];
-    check(&stuff, 3, 5, 0.5);
+    let stuff: Vec<f64> = vec![2.0, 3.0, 5.0, 2.0, 4.5, 2.0, 0.0, 0.0, 0.0, 5.0, 6.0, 4.0, 3.0, 5.5, 7.0];
+    println!("{:?}", iterate(&stuff, 3, 5, 0.00001));
 }
