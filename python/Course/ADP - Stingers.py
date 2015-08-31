49a9419ff7dc758631f8f7125921e646590e7b3c
Ravi Shankar <wafflespeanut@gmail.com>
2015-08-31 15:24:24 +0530

    correction...

--- python/Course/ADP - Stingers.py
+++ python/Course/ADP - Stingers.py
@@ -8,28 +8,28 @@ points = [(0.285, 0.285), (1.425, 0.57), (1.425, 0.57),
           (1.425, -0.285), (0.475, -0.19)]
 
 total = len(points)
-constant_stingers = [1, 2, 8, 9, 14, 15, 20, 21]
-initial_area = [0.000748 for i in range(total)]
+stingers = [1, 2, 8, 9, 14, 15, 20, 21]
+A = [3.4258e-04 if i not in stingers else 0.000748 for i in range(total)]
 
-A = [2.58e-03 if i not in constant_stingers else initial_area[i] for i in range(total)]
+Ixx, Iyy, Ixy = 1.706548e-07, 1.706548e-07, 0.707591e-07
 
 Ax = [A[i] * points[i][0] for i in range(total)]
 Ay = [A[i] * points[i][1] for i in range(total)]
-
-Ixx = [Ay[i] * points[i][1] for i in range(total)]
-Iyy = [Ax[i] * points[i][0] for i in range(total)]
-Ixy = [Ax[i] * points[i][1] for i in range(total)]
 
 Xc, Yc = sum(Ax) / sum(A), sum(Ay) / sum(A)
 
 X_Xc = [points[i][0] - Xc for i in range(total)]
 Y_Yc = [points[i][1] - Yc for i in range(total)]
 
-Sx, Sy = 744413.5006, 8448.1
-Sigma = [((Ixy[i] * Sx - Ixx[i] * Sy) / (Ixx[i] * Iyy[i] - Ixy[i])) * (X_Xc)[i] +
-        ((Ixy[i] * Sy - Ixx[i] * Sx) / (Ixx[i] * Iyy[i] - Ixy[i])) * (Y_Yc)[i] for i in range(total)]
+Ixx_ = [Ixx + A[i] * Y_Yc[i] ** 2 for i in range(total)]
+Iyy_ = [Iyy + A[i] * X_Xc[i] ** 2 for i in range(total)]
+Ixy_ = [Ixy + A[i] * X_Xc[i] * Y_Yc[i] for i in range(total)]
+
+Sx, Sy = 744413.5006, 178265.8194
+Sigma = [((Ixy_[i] * Sx - Ixx_[i] * Sy) / (Ixx_[i] * Iyy_[i] - Ixy_[i] ** 2)) * (X_Xc)[i] +
+        ((Ixy_[i] * Sy - Iyy_[i] * Sx) / (Ixx_[i] * Iyy_[i] - Ixy_[i] ** 2)) * (Y_Yc)[i] for i in range(total)]
 
 for i in range(total):
-    print Iyy[i], '\t\t', Sigma[i] / 10 ** 6
+    print i + 1, '\t\t', Ixx_[i], '\t\t', Sigma[i] / 10 ** 6
 
-print '\nLeast value of Ixx:', min(Ixx)
+print '\nLeast value of Ixx:', min(Ixx_)
