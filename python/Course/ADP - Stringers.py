e04c6a93ca582efbab9cc2760c3b8d9616f73e4f
Ravi Shankar <wafflespeanut@gmail.com>
2015-09-13 20:01:02 +0530

    pretty values...

--- python/Course/ADP - Stringers.py
+++ python/Course/ADP - Stringers.py
@@ -1,22 +1,18 @@
 from math import pi
 
-points = [(0.285, 0.285), (1.425, 0.57), (1.425, 0.57),
-          (2.185, 0.617), (3.23, 0.665), (4.18, 0.617),
-          (5.13, 0.57), (6.175, 0.52), (6.65, 0.475),
-          (6.65, 0.475), (7.41, 0.38), (8.17, 0.285),
-          (9.025, 0.095), (8.075, -0.095), (6.65, -0.2375),
-          (6.65, -0.2375), (6.08, -0.285), (4.75, -0.38),
-          (3.42, -0.42), (2.28, -0.38), (1.425, -0.285),
-          (1.425, -0.285), (0.475, -0.19)]
+points = [(0.285, 0.285), (1.425, 0.57), (2.185, 0.617), (3.23, 0.665), (4.18, 0.617),
+          (5.13, 0.57), (6.175, 0.52), (6.65, 0.475), (7.41, 0.38), (8.17, 0.285),
+          (9.025, 0.095), (8.075, -0.095), (6.65, -0.2375), (6.08, -0.285), (4.75, -0.38),
+          (3.42, -0.42), (2.28, -0.38), (1.425, -0.285), (0.475, -0.19)]
 
 total = len(points)
 stringer_area = 2.27741935e-3
-
-spar_front = [1, 2, 20, 21]
-spar_rear = [8, 9, 14, 15]
+spar_front, spar_rear = [1, 17], [7, 12]
 
 if 'stringer_area' in locals():
-    A = [0.0004645 if i in spar_front else 0.0004 if i in spar_rear else stringer_area for i in range(total)]
+    A = [0.0036946 if i in spar_front
+                   else 0.0034003 if i in spar_rear
+                                  else stringer_area for i in range(total)]
 else:
     A = [stringer_area for i in range(total)]
 
@@ -40,10 +36,22 @@ Sx, Sy = 11391789.36, 178265.8194
 Sigma = [((Ixy * Sx - Ixx * Sy) / (Ixx * Iyy - Ixy ** 2)) * (X_Xc)[i] +
         ((Ixy * Sy - Iyy * Sx) / (Ixx * Iyy - Ixy ** 2)) * (Y_Yc)[i] for i in range(total)]
 
-for i in range(total):
-    print i + 1, '\t\t', Ixx, '\t\t', Sigma[i] / 10 ** 6
+red, blue_1, blue_2, green, yellow, null = ('\033[' + str(i) + 'm' for i in (91, 96, 94, 92, 93, 0))
 
-print '\nLeast value of Ixx:', min(Ixx_)
-print '\nMax value of Sigma:', max(map(abs, Sigma)) / 10 ** 6
-print 'Critical Sigma:', ((pi ** 2) * 70e9 * min(Ixx_) / (stringer_area * 1.2 ** 2)) / 10 ** 6
+print
+for i in range(total):
+    a, b = blue_1, null
+    if i not in spar_front + spar_rear:
+        a, b = '', ''
+    print "  {}{}\t{}{}".format(a, i + 1, Sigma[i] / 10 ** 6, b)
+
+print '\n {}Least value of Ixx: {}{}\n'.format(blue_2, min(Ixx_), null)
+
+max_sigma = max(map(abs, Sigma)) / 10 ** 6
+critical_sigma = ((pi ** 2) * 70e9 * min(Ixx_) / (stringer_area * 1.4 ** 2)) / 10 ** 6
+
+color = red if max_sigma >= critical_sigma else green
+
+print ' {}Max value of Sigma: {} MPa{}'.format(color, max_sigma, null)
+print ' {}Critical Sigma: {} MPa{}'.format(yellow, critical_sigma, null)
 print
