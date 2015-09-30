b07719cda4903bfa184e3217d63bca718739c984
Ravi Shankar <wafflespeanut@gmail.com>
2015-10-01 01:55:27 +0530

    Falling back to approx. method... (sigh)

--- python/Course/ADP - Stringers.py
+++ python/Course/ADP - Stringers.py
@@ -29,22 +29,31 @@ def stringers(nodes, dist_list, airfoil_data, sep):
         resolved.append((x, y))
     return resolved
 
-def form_stringers(n_top = stringers_top, n_bottom = stringers_bottom, distance_required = False):
-    def slope(p1, p2):
-        try:
-            return (p2[1] - p1[1]) / (p2[0] - p1[0])
-        except ZeroDivisionError:       # slope's undefined for a vertical line
-            return None                 # which is how we're splitting the airfoil data
-
-    split_index = [slope(data[i], data[i + 1]) for i in range(len(data) - 1)].index(None)
-    airfoil_top, airfoil_bottom = data[:split_index + 1], data[split_index + 1:]
+def form_stringers(distance_required = False, n_top = stringers_top, n_bottom = stringers_bottom):
+    def split(data):
+        for i, point in enumerate(data):
+            if point[1] < 0:
+                return i
 
     def dist(p1, p2):
         return ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
 
+    def find_dist(points):
+        return [dist(points[i], points[i + 1]) for i in range(len(points) - 1)]
+
+    if distance_required:   # to find the approx. inter-stringer distance everywhere except behind the rear spar
+        st_data = get_points(out, 'FLANGE')
+        split_idx = split(st_data)
+        sd_top = find_dist([(0, 0)] + st_data[:split_idx])
+        sd_bottom = find_dist(st_data[split_idx:] + [(0, 0)])
+        return sd_top + sd_bottom
+
+    split_index = split(data)
+    airfoil_top, airfoil_bottom = data[:split_index + 1], data[split_index + 1:]
+
     # generate the lengths of line segments
-    dist_top = [dist(airfoil_top[i], airfoil_top[i + 1]) for i in range(len(airfoil_top) - 1)]
-    dist_bottom = [dist(airfoil_bottom[i], airfoil_bottom[i + 1]) for i in range(len(airfoil_bottom) - 1)]
+    dist_top = find_dist(airfoil_top)
+    dist_bottom = find_dist(airfoil_bottom)
     # find the separation for placing the given stringers
     sep_top, sep_bottom = sum(dist_top) / (n_top + 1), sum(dist_bottom) / (n_bottom + 1)
     print '\n Spacing at the top (m):', sep_top * chord, '\n Spacing at the bottom (m):', sep_bottom * chord
@@ -54,40 +63,6 @@ def form_stringers(n_top = stringers_top, n_bottom = stringers_bottom, distance_
     # resolve the points from the curve to the horizontal to find their absolute position
     resolved_top = stringers(nodes_top, dist_top, airfoil_top, sep_top)
     resolved_bottom = stringers(nodes_bottom, dist_bottom, airfoil_bottom, sep_bottom)
-
-    def collinear_points(start_point, end_point, middle):
-        (x1, y1), (x2, y2), (x0, y0) = start_point, end_point, middle
-        try:
-            return round((x0 - x1) / (x2 - x1), 6) == round((y0 - y1) / (y2 - y1), 6)
-        except ZeroDivisionError:
-            return False
-
-    def compare_dist(list_to_check, generated_list):    # to find the inter-stringer distance except behind the rear spar
-        generated_list = map(lambda point: (point[0] * chord, point[1] * chord), generated_list)
-        i, j, dist_list = 0, 0, []          # Scale everything up!
-        while i < len(list_to_check) - 1:
-            total_dist = 0
-            while j < len(generated_list) - 1 and \
-                  'FLANGE' not in list_to_check[i] and \
-                  not collinear_points(generated_list[j], generated_list[j + 1], list_to_check[i]):
-                total_dist += dist(generated_list[i], generated_list[i + 1])
-                j += 1
-            if 'FLANGE' in list_to_check[i]:
-                dist_list.append(dist(list_to_check[i], list_to_check[i + 1]))
-            elif total_dist:
-                dist_list.append(total_dist)
-            i += 1
-        return dist_list
-
-    if distance_required:
-        split_index = 0
-        st_data = get_points(out, 'FLANGE')
-        for i, point in enumerate(data):
-            if point[1] < 0:
-                split_index = i
-                break
-        print compare_dist([(0, 0)] + st_data[:split_index], [(0, 0)] + resolved_top)
-        return None
 
     def find_y(x, p1, p2):              # find `y` for a given `x` lying on the line joining two points
         (x0, y0), (x1, y1) = p1, p2
@@ -117,7 +92,7 @@ def form_stringers(n_top = stringers_top, n_bottom = stringers_bottom, distance_
     raw_input('\n {}NOTE:{} Data has been written to "{}"! Continue after checking the flanges...\n'.format(yellow, null, out))
     stringer_calc()
 
-def stringer_calc():
+def stringer_calc(dist_list):
     flange_pos, spar_front, spar_rear = [], [], []
     points = get_points("STRINGERS.dat", 'FLANGE')
     total = len(points)
@@ -157,11 +132,13 @@ def stringer_calc():
     Sigma = [((Ixy * Sx - Ixx * Sy) / (Ixx * Iyy - Ixy ** 2)) * (X_Xc)[i] +
             ((Ixy * Sy - Iyy * Sx) / (Ixx * Iyy - Ixy ** 2)) * (Y_Yc)[i] for i in range(total)]
 
+    print '  ID\t(inter-dist.)\tIxx\t\tSigma'
     for i in range(total):
         a, b = blue_1, null
         if i not in spar_front + spar_rear:
             a, b = '', ''
-        print "\t{}{}\t{}\t{}{}".format(a, i + 1, Ixx_[i], Sigma[i] / 10 ** 6, b)
+        print "  {}{}\t{}\t{}\t{}{}" \
+              .format(a, i + 1, dist_list[i], round(Ixx_[i], 8), round(Sigma[i] / 10 ** 6, 8), b)
 
     max_sigma = max(map(abs, Sigma)) / 10 ** 6
     critical_sigma = ((pi ** 2) * 150e9 * min(Ixx_) / (stringer_area * 1.4 ** 2)) / 10 ** 6
@@ -179,8 +156,8 @@ while True:
         2. Regenerate, by entering the values for the number of stringers (both top and bottom) and [Enter]...\n\
         4. Ctrl-C to quit!\n')
         if something == '':
-            form_stringers(500, 500, True)    # Meshing based on the airfoil data, just to ensure approximation
-            stringer_calc()
+            dist_list = form_stringers(True)
+            stringer_calc(dist_list)
         else:
             try:
                 s = something.split()
