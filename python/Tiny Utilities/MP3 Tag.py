e2652c3c4771bd66790622779a378e00e42aeb10
Ravi Shankar <wafflespeanut@gmail.com>
2015-10-15 00:20:43 +0530

    Updated for more beautification...

--- python/Tiny Utilities/MP3 Tag.py
+++ python/Tiny Utilities/MP3 Tag.py
@@ -4,29 +4,44 @@ import os, eyed3
 # I'm a perfectionist!
 
 path = os.path.expanduser('~/Desktop/TEMP')
-files_list = search(path, '.mp3')
 
 def check_name(string, is_filename = False):
-    avoid_upper = ['the', 'for', 'a', 'an', 'if', 'of', 'to', 'is']
+    avoid_upper = ['the', 'for', 'a', 'an', 'if', 'is', 'on',
+                   'in', 'by', 'am', 'not', 'of', 'to', 'at']
+    subs = {'and': '&'}
+    avoid_start = ['the', 'an', 'a']
     avoid_puncs = '"#$%*+./:;<=>?@[\\]^_`{|}~'     # possible symbols to be avoided for MP3 filenames
-    corrected = [word.lower() if (word.lower() in avoid_upper and i > 0) \
-                              else word.upper() for i, word in enumerate(string.split())]
+    split_list = string.split()
+    lim = len(split_list) - 1
+    corrected = [word.lower() if (word.lower() in avoid_upper and i > 0 and i < lim)
+                    else subs[word.lower()] if (word.lower() in subs and i > 0)
+                        else word.title() for i, word in enumerate(split_list)]
     if is_filename:
-        corrected = [''.join([s for s in word if s not in avoid_puncs])\
-                     for word in string.split()]
-    return ' '.join(corrected)
+        corrected = [''.join([s for s in word if s not in avoid_puncs])
+                        for i, word in enumerate(corrected)
+                            if (i > 0 or word.lower() not in avoid_start)]
+    new_string = ' '.join(corrected)
+    if "'" in new_string:
+        sliced = new_string.split("'")
+        new_string = sliced[0] + "'" + sliced[1][0].lower() + sliced[1][1:]
+    return new_string
 
-def get_metadata(files = files_list):
-    for file_path in files:
-        meta_data = {}
-        tag = eyed3.load(file_path).tag
-        artist, album, title, year = tag.artist, tag.album, tag.title, tag.best_release_date.year
-        filename, ext = os.path.basepath(file_path).split('.')
-        dirname = os.path.dirname(file_path)
-        album = ' '.join(map(lambda string: string.upper(), album.split()))
-        artist = ' '.join(map(lambda string: string.upper(), artist.split()))
-        title = check_name(title)
-        new_name = check_name(filename, True)
-        meta_data[filename] = dict([('name', new_name) \
-                                    ('artist', artist), ('album', album), \
-                                    ('title', title), ('year', year)])
+# def get_metadata(path = path):
+meta_data = []
+files = search(path, '.mp3')
+for file_path in files:
+    tag = eyed3.load(file_path).tag
+    year = tag.best_release_date.year
+    filename = os.path.basename(file_path).split('.')[0]
+    album = ' '.join(map(lambda string: string.title(), tag.album.split()))
+    artist = ' '.join(map(lambda string: string.title(), tag.artist.split()))
+    title = check_name(tag.title)
+    new_name = check_name(filename, True)
+    string = "\"{}\": {{\n\t"
+    string += ', '.join(("'name': \"{}\"",
+                         "\n\t'title': \"{}\"", "'album': \"{}\"",
+                         "\n\t'artist': \"{}\"", "'year': {},"))
+    string += '\n}}'
+    meta_data.append(string.format(file_path, new_name, title, album, artist, year))
+with open('TEMP.json', 'w') as File:
+    File.write('{\n' + ',\n'.join(meta_data) + '\n}')
