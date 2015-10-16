854580fa59b3d35e9247478c7757b4dfa45515f0
Ravi Shankar <wafflespeanut@gmail.com>
2015-10-16 13:12:06 +0530

    Phew, finally... we're able to tag!

--- python/Tiny Utilities/MP3 Tag.py
+++ python/Tiny Utilities/MP3 Tag.py
@@ -1,11 +1,10 @@
 execfile('Python Cleanup.py')
-import os, eyed3
+import eyed3, os, sys
 
-# I'm a perfectionist!
-
+# A script to cleanup my soundtracks...
 path = os.path.expanduser('~/Desktop/TEMP')
 
-def check_name(string, is_filename = False):
+def check_name(string, is_filename = False):    # custom names and titles for tracks
     avoid_upper = ['the', 'for', 'a', 'an', 'if', 'is', 'on',
                    'in', 'by', 'am', 'not', 'of', 'to', 'at']
     subs = {'and': '&'}
@@ -26,22 +25,70 @@ def check_name(string, is_filename = False):
         new_string = sliced[0] + "'" + sliced[1][0].lower() + sliced[1][1:]
     return new_string
 
-# def get_metadata(path = path):
-meta_data = []
-files = search(path, '.mp3')
-for file_path in files:
-    tag = eyed3.load(file_path).tag
-    year = tag.best_release_date.year
-    filename = os.path.basename(file_path).split('.')[0]
-    album = ' '.join(map(lambda string: string.title(), tag.album.split()))
-    artist = ' '.join(map(lambda string: string.title(), tag.artist.split()))
-    title = check_name(tag.title)
-    new_name = check_name(filename, True)
-    string = "\"{}\": {{\n\t"
-    string += ', '.join(("'name': \"{}\"",
-                         "\n\t'title': \"{}\"", "'album': \"{}\"",
-                         "\n\t'artist': \"{}\"", "'year': {},"))
-    string += '\n}}'
-    meta_data.append(string.format(file_path, new_name, title, album, artist, year))
-with open('TEMP.json', 'w') as File:
-    File.write('{\n' + ',\n'.join(meta_data) + '\n}')
+def get_metadata(path):           # walk over a given path and get the metadata of MP3 files
+    print
+    meta_data = []
+    files = search(path, '.mp3')
+    i, total = 1, len(files)
+    for file_path in files:         # title, album, artist & year is what we need
+        tag = eyed3.load(file_path).tag
+        filename = os.path.basename(file_path).split('.')[0]
+        year = tag.best_release_date.year if tag.best_release_date else None
+        album = ' '.join(map(lambda string: string.title(), tag.album.split())) if tag.album else None
+        artist = ' '.join(map(lambda string: string.title(), tag.artist.split())) if tag.artist else None
+        title = check_name(tag.title) if tag.title else check_name(filename)
+        new_name = check_name(title, True)
+        string = "%r: {\n\t"       # write a viewable dictionary to file
+        string += ', '.join(("'name': %r",
+                             "\n\t'title': %r", "'album': %r",
+                             "\n\t'artist': %r", "'year': %r,"))
+        string += '\n}'
+        meta_data.append(string % (file_path, new_name, title, album, artist, year))
+        sys.stdout.write('\rReading files... (%d/%d)' % (i, total))
+        i += 1
+    with open('TEMP.json', 'w') as File:
+        File.write('{\n\n' + ',\n'.join(meta_data) + '\n\n}')
+
+def change_meta(path = path):
+    get_metadata(path)
+    raw_input('\nMetadata has been dumped! Continue after making changes... ')
+    with open('TEMP.json') as File:
+        data = File.read()
+    while True:
+        try:
+            errors = []
+            exec('meta_data = ' + data)     # awesomesauce of interpreters!
+            stuff = meta_data.items()
+            i, total = 1, len(stuff)
+            for file_path, data in stuff:
+                year = data['year']
+                all_data = { 'Album name': data['album'], 'Artist name': data['artist'], 'Year': year }
+                for attr, val in all_data.items():
+                    if not val:
+                        err = '%s not found for %s' % (attr, file_path)
+                        errors.append(err)
+                if year and type(year) is not int:
+                    try:
+                        data['year'] = int(year)
+                    except ValueError:
+                        errors.append('Year should be in the form of numbers for %s' % file_path)
+            if not errors:
+                break
+            print '\n' + '\n'.join(errors)
+            raw_input('\nErrors found! Continue after fixing them...')
+            with open('TEMP.json') as File:
+                data = File.read()
+        except Exception:
+            print '\nCannot parse some of the values. Make sure that all the data are in strings!'
+            raw_input('Continue after fixing it...')
+
+    for file_path, data in stuff:
+        tag = eyed3.load(file_path).tag
+        tag.clear()
+        tag.album, tag.artist, tag.title = unicode(data['album']), unicode(data['artist']), unicode(data['title'])
+        tag.release_date, tag.recording_date, tag.original_release_date = [data['year']] * 3
+        tag.save(file_path)
+        file_dir = os.path.dirname(file_path)
+        os.rename(file_path, os.path.join(file_dir, data['name'] + '.mp3'))
+    os.remove('TEMP.json')
+    print 'Yay! Cleaned up everything!'
