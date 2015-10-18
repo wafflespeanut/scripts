f981c4ede45e768c59c44aa2a202dc6d1955a406
Ravi Shankar <wafflespeanut@gmail.com>
2015-10-18 22:10:44 +0530

    album covers for tagging...

--- python/Tiny Utilities/MP3 Tag.py
+++ python/Tiny Utilities/MP3 Tag.py
@@ -1,10 +1,10 @@
 execfile('Python Cleanup.py')
-import eyed3, os, sys
+import eyed3, os, sys, shutil
 from PIL import Image
 
 # A script to cleanup my soundtracks...
 path = os.path.expanduser('~/Desktop/TEMP')
-
+temp_loc = os.path.join(path, 'TEMP')
 colors = { 'R': '91', 'G': '92', 'Y': '93', 'B2': '94', 'P': '95', 'B1': '96', 'W': '97', '0': '0', }
 
 def fmt(color = '0'):
@@ -12,19 +12,19 @@ def fmt(color = '0'):
 
 def split_and_strip(string, puncs = '/;,'):     # filter common separators for artists
     split_strip = map(lambda s: s.strip(), string.split(puncs[0]))
-    if len(puncs) == 1:
+    if puncs == puncs[-1]:
         return split_strip
     else:
         new_string = puncs[1].join(split_strip)
     return split_and_strip(new_string, puncs[1:])
 
 def check_name(string, is_filename = False):    # custom names and titles for tracks
-    avoid_titles = ['the', 'for', 'a', 'an', 'if', 'is', 'on',   # I hate when these have title cases
-                    'in', 'by', 'am', 'not', 'of', 'to', 'at']
+    avoid_titles = ['the', 'for', 'a', 'an', 'if', 'is', 'on', 'or',
+                    'in', 'by', 'am', 'not', 'of', 'to', 'at', 'be']
     subs = {'and': '&'}     # I also prefer to substitute
     avoid_start = ['the', 'an', 'a']            # I prefer filenames to not begin with articles
-    allowed_puncs = '!&\'(),-'                  # symbols that can be allowed for track titles
-    avoid_puncs = '"#$%*+./:;<=>?@[\\]^_`{|}~'  # possible symbols to be avoided for MP3 filenames
+    allowed_puncs = '!&\'(),-.'                 # symbols that can be allowed for track titles
+    avoid_puncs = '"#$%*+/:;<=>?@[\\]^_`{|}~'   # possible symbols to be avoided for MP3 filenames
 
     split_list = string.split()
     lim = len(split_list) - 1
@@ -37,8 +37,7 @@ def check_name(string, is_filename = False):    # custom names and titles for tr
         elif i > 0 and lower in subs:
             corrected.append(subs[lower])
         else:
-            corrected.append(word[0].upper() + word[1:])
-
+            corrected.append(word.capitalize())
     if is_filename:
         corrected = [''.join([s for s in word if s not in avoid_puncs])
                         for i, word in enumerate(corrected)
@@ -46,10 +45,28 @@ def check_name(string, is_filename = False):    # custom names and titles for tr
     new_string = ' '.join(corrected)
     return new_string
 
+def check_image(file_path, size = 400):
+    if not os.path.exists(file_path):
+        return False, "Image doesn't exist at the given location! ({})".format(file_path)
+    img = Image.open(file_path)
+    width, height = img.size
+    if width == height:
+        if width < size:
+            return False, 'Image size should be at least %s!' % size
+        elif width > size:
+            img.thumbnail((size, size), Image.ANTIALIAS)
+            img.save(file_path)
+        return True, None
+    return False, 'Image width & height should be equal!'
+
 def get_metadata(path):           # walk over a given path and get the metadata of MP3 files
     print
     meta_data = []
-    files = search(path, '.mp3')
+    image_data = {}     # only for storing the cover image path
+    mime = { 'image/jpeg': '.jpg', 'image/png': '.png' }
+    files = search(path, '.mp3') + search(path, '.MP3')
+    if not files:
+        return False
     i, total = 1, len(files)
     for file_path in files:         # title, album, artist & year is what we need
         tag = eyed3.load(file_path).tag
@@ -59,47 +76,73 @@ def get_metadata(path):           # walk over a given path and get the metadata 
         title = check_name(tag.title) if tag.title else check_name(filename)
         year = tag.best_release_date.year if tag.best_release_date else ''
         new_name = check_name(title, True)
+
+        if album not in image_data or image_data[album] == 'NONE':
+            image_path = os.path.join(temp_loc, album)
+            if tag.images:
+                image_path += mime[tag.images[0].mime_type]
+                with open(image_path, 'wb') as File:
+                    File.write(tag.images[0].image_data)
+                image_data[album] = image_path
+        elif album not in image_data:
+            image_data[album] = 'NONE'
+
         string = u'"{}": {{\n\t'    # write a viewable dictionary to file
-        string += u', '.join((u'"name": "{}"',
-                              u'\n\t"title": "{}"', u'"album": "{}"',
-                              u'\n\t"artist": "{}"', u'"year": "{}"',))
+        string += ',\n\t'.join((u'"name": "{}"', u'"cover": "{}"',
+                                u'"title": "{}"', u'"album": "{}"',
+                                u'"artist": "{}"', u'"year": "{}",',))
         string += u'\n}}'
-        meta = string.format(file_path.decode('utf-8'), new_name, title, album, artist, year)
+        meta = string.format(file_path.decode('utf-8'), new_name, image_data[album], title, album, artist, year)
         meta_data.append(meta)
-        sys.stdout.write('\rReading files... (%d/%d)' % (i, total))
+        sys.stdout.write('\r Reading files... (%d/%d)' % (i, total))
         i += 1
-    with open('TEMP.json', 'w') as File:
-        meta_data = u'{\n\n' + u',\n'.join(meta_data) + u'\n\n}'
+    with open(os.path.join(temp_loc, 'TEMP.json'), 'w') as File:
+        meta_data = ',\n'.join(meta_data)
         File.write(meta_data.encode('utf-8'))
+    return True
 
 def change_meta(path = path):       # get the dumped metadata and modify the files
-    get_metadata(path)
+    eyed3.log.setLevel("ERROR")
+    if not os.path.exists(temp_loc):
+        os.mkdir(temp_loc)
+    if not get_metadata(path):
+        print '%s Nothing to work!%s' % (fmt('R'), fmt())
+        return
     raw_input('\nMetadata has been dumped! Continue after making changes...')
     while True:
-        with open('TEMP.json') as File:
+        with open(os.path.join(temp_loc, 'TEMP.json')) as File:
             data = File.read().decode('utf-8')
         try:
             print
             errors = []
-            exec('meta_data = ' + data)     # awesomesauce!
+            exec('meta_data = {' + data + '}')     # Look at this awesomesauce!
             stuff = meta_data.items()
             i, total = 1, len(stuff)
             for file_path, data in stuff:
                 decoded_path = file_path.decode('utf-8')
-                try:
+                try:        # yeah, I'm being strict here - all these attributes are necessary for me
                     year = data['year']
-                    all_data = { 'Album name': data['album'], 'Artist name': data['artist'], 'Year': year }
+                    image_path = data['cover']      # cover is optional, so let's just pretend that we have it
+                    data_req = { 'Album name': data['album'],
+                                 'Album cover': image_path,
+                                 'Artist name': data['artist'],
+                                 'Year': year }
                 except KeyError as err:
                     errors.append((decoded_path, "Missing '%s'!" % err.args[0]))
-                for attr, val in all_data.items():
-                    if not val:     # yeah, I'm being strict here - all these attributes are necessary
+                for attr, val in data_req.items():
+                    if not val:
                         errors.append((decoded_path, '%s not found!' % attr))
                 if year:
                     try:
-                        data['year'] = int(year)
+                        data['year'] = int(year)        # check if a valid year is given
                     except ValueError:
                         errors.append((decoded_path, 'Year should be in the form of numbers!'))
-                sys.stdout.write('\rChecking files... (%d/%d)' % (i, total))
+                image_path = None if image_path == 'NONE' else image_path
+                if image_path:                          # check the cover image
+                    success, fail_reason = check_image(image_path)
+                    if not success:
+                        errors.append((decoded_path, fail_reason))
+                sys.stdout.write('\r Checking files... (%d/%d)' % (i, total))
                 i += 1
             if not errors:
                 break
@@ -114,16 +157,22 @@ def change_meta(path = path):       # get the dumped metadata and modify the fil
 
     i = 1
     print
+    mime = { '.jpg': 'image/jpeg', '.png': 'image/png' }
     for file_path, data in stuff:
         tag = eyed3.load(file_path).tag
         tag.clear()
         tag.album, tag.artist, tag.title = map(lambda s: s.decode('utf-8'),
                                                (data['album'], data['artist'], data['title']))
         tag.release_date, tag.recording_date, tag.original_release_date = [data['year']] * 3
+        if image_path:
+            ext = image_path[image_path.rfind('.'):]
+            with open(image_path, 'rb') as image:
+                tag.images.set(3, image.read(), mime[ext], tag.album)
         tag.save(file_path)
         file_dir = os.path.dirname(file_path)
-        os.rename(file_path, os.path.join(file_dir, data['name'] + '.mp3'))
-        sys.stdout.write('\rApplying attributes... (%d/%d)' % (i, total))
+        filename = os.path.join(file_dir, data['name'] + ' ({}).mp3'.format(tag.album))
+        os.rename(file_path, filename)
+        sys.stdout.write('\r Applying attributes... (%d/%d)' % (i, total))
         i += 1
-    os.remove('TEMP.json')
+    shutil.rmtree(temp_loc)
     print '\n\n{}Yay! Cleaned up everything!{}'.format(fmt('G'), fmt())
--- python/Tiny Utilities/Philographics.py
+++ python/Tiny Utilities/Philographics.py
@@ -2,7 +2,7 @@ execfile("Download MP3s.py")
 from PIL import Image
 import os
 
-path = "C:\\Users\\Waffles Crazy Peanut\\Desktop\\Dropbox\\Philographics\\"
+path = os.path.expanduser('~/Desktop/Dropbox/Philographics/')
 link = "http://geniscarreras.com/philographics"
 
 def download():
@@ -10,17 +10,17 @@ def download():
         mkdir(path)
     for image in urls(link, 'jpg', '//img/@data-src'):
         name = image.split('/')[-1]
-        print 'Downloading',name,'...'
+        print 'Downloading', name, '...'
         urlretrieve(image, path + name)
         print 'Processing...'
         process(path + name, name)
 
-def process(image, name):                  # Some experiment for making wallpapers from those philographics
+def process(image, name):       # Some experiment for making wallpapers from those philographics
     img = Image.open(image)
     width, height = img.size
-    r, g, b = img.getpixel((width-1, height-1))                     # Pick color
-    width = int(height * float(1366)/768)
+    r, g, b = img.getpixel((width - 1, height - 1))             # Pick color
+    width = int(height * float(1366) / 768)
     newImg = Image.new(img.mode, (width, height), (r, g, b))
-    offset = (newImg.size[0] - img.size[0])/2                       # Position: center
+    offset = (newImg.size[0] - img.size[0]) / 2                 # Position: center
     newImg.paste(img, (offset, 0))
     newImg.save(path + name)
