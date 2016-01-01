09ae5f70366cae105d3f9063caf5dcf4ec961d81
Ravi Shankar <wafflespeanut@gmail.com>
2016-01-01 15:52:10 +0530

    updated the tagger for using custom names

--- python/mp3-tag.py
+++ python/mp3-tag.py
@@ -1,6 +1,9 @@
-import eyed3, os, sys, shutil
-from PIL import Image
-from time import sleep
+try:
+    import eyed3, os, sys, shutil
+    from PIL import Image
+    from time import sleep
+except ImportError:
+    print "You'll be needing 'PIL' & 'eyed3' modules for this thing!"
 
 # A script to cleanup my soundtracks...
 # It generates a json file, parses that as a dictionary and applies it to the tracks
@@ -18,8 +21,8 @@ colors = { 'R': '91', 'G': '92', 'Y': '93', 'B2': '94', 'P': '95', 'B1': '96', '
 def fmt(color = '0'):
     return {'win32': ''}.get(sys.platform, '\033[' + colors[color] + 'm')
 
-avoid_titles = ['in', 'by', 'am', 'a', 'an', 'if', 'is', 'on', 'or',
-                'the', 'for', 'not', 'of', 'to', 'at', 'be', 'vs']
+avoid_titles = ['in', 'by', 'am', 'a', 'an', 'if', 'is', 'on', 'or', 'not',
+                'the', 'for',  'of', 'to', 'at', 'be', 'from', 'vs']
 # I prefer punctuations over some words and hyphen over some punctuations
 word_subs = { 'and': '&' }
 prefer_hyphen = ':_/'
@@ -31,7 +34,7 @@ avoid_puncs = '"%*+/:;<=>?@[\\]^_`{|}~'     # symbols to be avoided for filename
 def get_caps(string):
     return ''.join(s for s in string if s.isupper() or s.isdigit() or s in allowed_puncs)
 
-def split_and_strip(string, puncs = '/;,'):     # filter common separators for artists
+def split_and_strip(string, puncs = '/;,'):     # filter common separators for words
     split_strip = map(lambda s: s.strip(), string.split(puncs[0]))
     if puncs == puncs[-1]:
         return split_strip
@@ -85,12 +88,26 @@ def get_metadata(path):     # walk over a given path and get the metadata of MP3
     if not files:
         return False
     i, total = 1, len(files)
+
+    def attr_from_comments(tag, attr_val, idx, check_func, alternative = lambda _val: ''):
+        if tag.comments:    # We can be quite sure here, because we're gonna erase everything else
+            attrs = tag.comments[0].text.split('|||')   # our separator for those attributes
+            if len(attrs) > idx:
+                if attr_val and attrs[idx] and attr_val == attrs[idx]:
+                    return attr_val, 'true'
+        return check_func(attr_val) if attr_val else alternative(attr_val), 'false'
+
     for file_path in files:         # title, album, artist & year is what we need
         tag = eyed3.load(file_path).tag
         filename = os.path.basename(file_path).split('.')[0]
         album = check_name(tag.album) if tag.album else ''
         artist = '/'.join(map(check_name, split_and_strip(tag.artist))) if tag.artist else ''
-        title = check_name(tag.title) if tag.title else check_name(filename)
+        # If we wanna force the title, album or artist (i.e., don't want the machine output)
+        # we can store them in the comments and check against the existing tag attributes
+        title, forced_title = attr_from_comments(tag, tag.title, 0, check_name, lambda _val: check_name(filename))
+        album, forced_album = attr_from_comments(tag, tag.album, 1, check_name)
+        artist, forced_artist = attr_from_comments(tag, tag.artist, 2,
+                                                  lambda val: '/'.join(map(check_name, split_and_strip(val))))
         year = tag.best_release_date.year if tag.best_release_date else ''
         new_name = check_name('%s - %s - %s' % (title, get_caps(album), get_caps(artist)), True)
 
@@ -108,16 +125,20 @@ def get_metadata(path):     # walk over a given path and get the metadata of MP3
 
         string = u'"{}": {{\n\t'    # write a viewable dictionary to file
         string += ',\n\t'.join((u'"name": "{}"', u'"cover": "{}"',
-                                u'"title": "{}"', u'"album": "{}"',
-                                u'"artist": "{}"', u'"year": "{}",',))
+                                u'"title": "{}"', u'"forced_title": {}',
+                                u'"album": "{}"', u'"forced_album": {}',
+                                u'"artist": "{}"', u'"forced_artist": {}',
+                                u'"year": {}'))
         string += u'\n}}'
         meta = string.format(file_path.decode('utf-8'), new_name,
                              image_data.get(lookup_key, 'NONE'),
-                             title, album, artist, year)
+                             title, forced_title, album, forced_album,
+                             artist, forced_artist, year)
         meta_data.append(meta)
         sys.stdout.write('\r Reading files... (%d/%d)' % (i, total))
         sys.stdout.flush()
         i += 1
+
     with open(os.path.join(temp_loc, 'TEMP.json'), 'w') as File:
         meta_data = ',\n'.join(meta_data)
         File.write(meta_data.encode('utf-8'))
@@ -135,33 +156,31 @@ def change_meta(rel_path = path, meta_exists = False):      # get the dumped met
         return
     if not meta_exists:
         raw_input('\nMetadata has been dumped! Continue after making changes...')
+
     while True:
         with open(json) as File:
             data = File.read().decode('utf-8')
         try:
             print
+            true, false = True, False
+            attrs = ['title', 'album', 'artist']
             errors = []
-            exec('meta_data = {' + data + '}')     # Look at this awesomesauce!
+            exec 'meta_data = {' + data + '}' in globals(), locals()    # awesomesauce!
             stuff = meta_data.items()
             i, total = 1, len(stuff)
             for file_path, data in stuff:
                 decoded_path = file_path.decode('utf-8')
                 try:        # yeah, I'm being strict here - all these attributes are necessary for me
-                    year = data['year']
-                    data_req = { 'Album name': data['album'],
-                                 'Album cover': data['cover'],
-                                 'Artist name': data['artist'],
-                                 'Year': year }
+                    assert int(data['year']) > 1900, "songs didn't exist back then!"
+                    for attr in attrs:
+                        assert type(data['forced_' + attr]) is bool, 'forced_%s should be a bool!' % attr
+                        assert data[attr], '%s not found!' % attr
                 except KeyError as err:
-                    errors.append((decoded_path, "Missing '%s'!" % err.args[0]))
-                for attr, val in data_req.items():
-                    if not val:     # cover is optional, so let's just pretend that we have it
-                        errors.append((decoded_path, '%s not found!' % attr))
-                if year:
-                    try:
-                        data['year'] = int(year)        # check if a valid year is given
-                    except ValueError:
-                        errors.append((decoded_path, 'Year should be in the form of numbers!'))
+                    errors.append((decoded_path, "missing '%s'!" % err.args[0]))
+                except ValueError:
+                    errors.append((decoded_path, 'year should be in the form of numbers!'))
+                except AssertionError as err:
+                    errors.append((decoded_path, err))
                 image_path = data['cover']
                 if image_path != 'NONE':                # check the cover image
                     success, fail_reason = check_image(image_path)
@@ -178,7 +197,7 @@ def change_meta(rel_path = path, meta_exists = False):      # get the dumped met
             raw_input('\nErrors found! Continue after fixing them...')
         except Exception as err:
             print err
-            print '%sCannot parse some of the values. Make sure that all the data are in strings!%s' \
+            print '%sCannot parse some of the values. Make sure that the data is in JSON format!%s' \
                    % (fmt('R'), fmt())
             raw_input('Continue after fixing it...')
 
@@ -188,14 +207,17 @@ def change_meta(rel_path = path, meta_exists = False):      # get the dumped met
     for file_path, data in stuff:
         tag = eyed3.load(file_path).tag
         tag.clear()
-        tag.album, tag.artist, tag.title = map(lambda s: s.decode('utf-8'),
-                                               (data['album'], data['artist'], data['title']))
-        tag.release_date, tag.recording_date, tag.original_release_date = [data['year']] * 3
+        attr_objs = tag.title, tag.album, tag.artist = map(lambda s: data[s].decode('utf-8'), attrs)
+        tag.release_date, tag.recording_date, tag.original_release_date = map(str, [data['year']] * 3)
         image_path = data['cover']
         if image_path != 'NONE':
             ext = image_path[image_path.rfind('.'):]
             with open(image_path, 'rb') as image:
                 tag.images.set(3, image.read(), mime[ext], tag.album)
+        # Too bad that I have to index stuff here...
+        comment = u'|||'.join(map(lambda idx: attr_objs[idx] if data['forced_' + attrs[idx]] else 'NONE',
+                                  range(len(attrs))))      # This doesn't work due to exec's limitation
+        tag.comments.set(comment)
         tag.save(file_path)
         file_dir = os.path.dirname(file_path)
         filename = os.path.join(file_dir, data['name'])
