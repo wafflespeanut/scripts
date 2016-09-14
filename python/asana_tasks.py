034c83f6588a09985478ce81516bd4999ed63067
Ravi Shankar <wafflespeanut@gmail.com>
2016-09-14 14:01:36 +0530

    AsanaTasksCreator class and config-based multi-task creation

--- python/asana_tasks.py
+++ python/asana_tasks.py
@@ -1,203 +1,130 @@
 from datetime import datetime
 
-import json, requests
-
-# Basic CUI to interact with Asana tasks (my job demands using Asana, and I'm lazy!)
-
-token = 'foobar123'
-BASE_URL = 'https://app.asana.com/api/1.0'
+import json, re, requests
 
 
-def request(method, url, data=None):
-    print '%s: %s' % (method, url)
-    headers = {'Authorization': 'Bearer %s' % token}
-    if method == 'POST':
-        headers['Content-type'] = 'application/json'
-        data = json.dumps(data)
+class AsanaTasksCreator(object):
+    '''
+    Basic methods to interact with Asana tasks
+    (my workplace demands using Asana for logging, and I'm lazy!)
+    '''
+    base_url = 'https://app.asana.com/api/1.0/'
+    proj_url = '/projects'
+    spaces_url = '/workspaces'
+    users_url = '/workspaces/%s/users'
+    tasks_url = '/tasks/%s'
+    root_tasks_url = '/projects/%s/tasks'
+    sub_tasks_url = '/tasks/%s/subtasks'
 
-    req_method = getattr(requests, method.lower())
-    resp = req_method(url, data=data, headers=headers)
-    obj = json.loads(resp.text)
-    return obj['data'] if obj.get('data') is not None else obj
+    def __init__(self, token):
+        self.token = token
 
+    def _request(self, method, rel_url, data=None):
+        url = self.base_url + rel_url.lstrip('/')
+        print '%s: %s' % (method, url)
+        headers = {'Authorization': 'Bearer %s' % self.token}
+        if method == 'POST':
+            headers['Content-type'] = 'application/json'
+            data = json.dumps(data)
 
-def get_date():
-    while True:
-        try:
-            date = raw_input("Due date in YYYY-MM-DD format (Press [Enter] for today, Press '!' to skip): ") or None
-            if date == '!':
-                return
-            elif not date:
-                return datetime.now().strftime('%Y-%m-%d')
+        req_method = getattr(requests, method.lower())
+        resp = req_method(url, data=data, headers=headers)
+        obj = json.loads(resp.text)
+        return obj['data'] if obj.get('data') is not None else obj
 
-            datetime.strptime(date, '%Y-%m-%d')
-            return date
-        except ValueError as err:
-            print 'Error: %s, Please try again...' % err
-        except KeyboardInterrupt:
+    def _get_stuff(self, url):
+        stuff = {}
+        for thing in self._request('GET', url):
+            stuff[thing['name']] = thing['id']
+        return stuff
+
+    def get_workspaces(self):
+        return self._get_stuff(self.spaces_url)
+
+    def get_projects(self):
+        return self._get_stuff(self.proj_url)
+
+    def get_users(self, space_id):
+        return self._get_stuff(self.users_url % space_id)
+
+    def get_tasks(self, parent_id, is_root=False):
+        url = self.root_tasks_url if is_root else self.sub_tasks_url
+        return self._get_stuff(url % parent_id)
+
+    def create_task_data(**kwargs):
+        data = {}
+        if kwargs.get('space_id'):
+            data['workspace'] = kwargs['space_id']
+        if kwargs.get('proj_id'):
+            data['projects'] = [kwargs['proj_id']]
+        if kwargs.get('name'):
+            data['name'] = kwargs['name']
+        if kwargs.get('assignee'):
+            data['assignee'] = kwargs['assignee']
+        if kwargs.get('completed'):
+            data['completed'] = kwargs['completed']
+        if kwargs.get('date'):
+            date = datetime.strptime(kwargs['date'], '%Y-%m-%d')
+            data['due_on'] = date.strftime('%Y-%m-%d')
+        return data
+
+    def update_task(self, task_id, is_root=False, **kwargs):
+        data = self.create_task_data(**kwargs)
+        if not data:
             return
 
+        url = self.tasks_url % task_id
+        self._request('PUT', task_id, data)
 
-def get_task_data(space_id, proj_id):
-    data = { 'workspace': space_id }
-    if proj_id:
-        data['projects'] = [proj_id]
-
-    name = raw_input('Name of your task: ')
-    if not name:
-        return
-
-    data['name'] = name
-    is_assigned = raw_input('Assign it to you (y/n)? ') == 'y'
-    if is_assigned:
-        data['assignee'] = 'me'
-
-    due_date = get_date()
-    if due_date:
-        data['due_on'] = due_date
-
-    is_complete = raw_input('Is it done (y/n)? ') == 'y'
-    if is_complete:
-        data['completed'] = is_complete
-
-    return data
-
-
-def get_input_for_opts(options, prompt, sort_key=None, print_val=True):
-    options = sorted(options, key=sort_key)
-    for i, opt in enumerate(options):
-        val = sort_key(opt) if sort_key and print_val else opt
-        print '%d. %s' % (i + 1, val)
-
-    while True:
-        try:
-            string = raw_input('\n%s ' % prompt)
-            if not string:
-                return
-
-            val = int(string)
-            assert val >= 1, "value should be at least 1"
-            assert val <= len(options), "value should be at most %s" % len(options)
-            return options[val - 1] if sort_key and print_val else val - 1
-        except Exception as err:
-            print 'Error: %s, Please try again...' % err
-        except KeyboardInterrupt:
-            print
+    def create_new_task(self, parent_id, is_root=False, **kwargs):
+        if not (kwargs['name'] and kwargs['space_id']):
             return
 
-
-# Recursively traverse through tasks, sub-tasks, sub-sub-tasks, etc.
-def task_recursion(space_id, proj_id, task={}, level=0):
-    print '\n(Task recursion level: %d)' % level
-    options = {
-        'Assign this %stask to you' % ('sub-' * (level - 1)): 0,
-        'Set due date for this %stask' % ('sub-' * (level - 1)): 1,
-        'Mark this %stask as completed' % ('sub-' * (level - 1)): 2,
-        'Rename this %stask' % ('sub-' * (level - 1)): 3,
-        'Create a new %stask' % ('sub-' * level): 4,
-        'Update an existing %stask' % ('sub-' * level): 5,
-    }
-
-    while True:     # so that we can jump back to the previous menu (on null input)
-        task_id, task_name = task.get('id'), task.get('name')
-        task_url = '%s/tasks/%s/subtasks' % (BASE_URL, task_id)
-        skip_first = 4
-
-        if level == 0:
-            if task:
-                task = {}
-                continue
-
-            options = dict(filter(lambda (_, v): v >= skip_first, options.items()))
-            task_url = '%s/projects/%s/tasks' % (BASE_URL, proj_id)
-
-        print ('Task: %s\n' % task_name) if task_name else ''
-        idx = get_input_for_opts(options, 'Input:', options.get, print_val=False)
-        if idx is None:
-            print 'Returning to previous menu...'
+        if is_root and not kwargs['proj_id']:
             return
 
-        idx += skip_first if level == 0 else 0
-
-        if idx == 0:
-            data = { 'assignee': 'me' }
-            request('PUT', '%s/tasks/%s' % (BASE_URL, task_id), data)
-            print 'Assigned to you!\n'
-
-        elif idx == 1:
-            date = get_date()
-            if not date:
-                return
-
-            data = { 'due_on': date }
-            request('PUT', '%s/tasks/%s' % (BASE_URL, task_id), data)
-            print 'Due date changed!\n'
-
-        elif idx == 2:
-            data = { 'completed': True }
-            request('PUT', '%s/tasks/%s' % (BASE_URL, task_id), data)
-            print 'Marked as completed!\n'
-
-        elif idx == 3:
-            name = raw_input('Enter a new name for the %stask: ' % ('sub-' * (level - 1)))
-            data = { 'name': name }
-            request('PUT', '%s/tasks/%s' % (BASE_URL, task_id), data)
-            print 'Renamed!\n'
-
-        elif idx == 4:
-            try:
-                data = get_task_data(space_id, proj_id)
-                if not data:
-                    return
-            except KeyboardInterrupt:
-                return
-
-            request('POST', task_url, { 'data': data })
-            print 'Created new task!\n'
-
-        elif idx == 5:
-            print '\n[List of %stasks]' % ('sub-' * level)
-            tasks = request('GET', task_url)
-            if not tasks:
-                raw_input('\nNo %stasks found! Press [Enter] to go back...' % ('sub-' * level))
-                return
-
-            while True:
-                choice = get_input_for_opts(tasks,
-                                            'Which %stask do you wanna interact with?' % ('sub-' * level),
-                                            lambda v: v['name'])
-                if not choice:
-                    print 'Returning to previous menu...'
-                    break
-
-                task = choice
-                task_recursion(space_id, None, task, level + 1)
+        data = self.create_task_data(**kwargs)
+        url = self.root_tasks_url if is_root else self.sub_tasks_url
+        self._request('POST', url % parent_id, data)
 
 
-def begin_battle(space_id=None):
-    user_id = request('GET', '%s/users/me' % BASE_URL)['id']
-    print 'User ID: %s\n' % user_id
+def begin_battle(config):
+    api = AsanaTasksCreator(config['token'])
+    space = config['space']
 
-    if not space_id:
-        workspaces = request('GET', '%s/workspaces' % BASE_URL)
-
-        for space in workspaces:
-            if 'genome' in space['name'].lower():
-                space_id = space['id']
-                print 'Workspace ID: %s\n' % space_id
+    if isinstance(space, str):
+        spaces = api.get_workspaces()
+        for name, space in spaces.items():
+            if re.search(config['space'], name):
                 break
 
-        if not space_id:
-            exit('Workspace not found!\n')
+    print 'Workspace ID: %d' % (space)
+    projects = api.get_projects()
 
-    proj_url = '%s/projects' % BASE_URL
-    projects = request('GET', proj_url)
+    for name, proj_inner in config['projects'].items():
+        for proj_name, proj_id in projects.items():
+            if name != proj_name:
+                continue
 
-    while True:
-        print
-        proj = get_input_for_opts(projects, 'Which project do you wanna interact with?',
-                                  lambda v: v['name'])
-        if not proj:
-            return
+            print 'Project: %s (ID: %d)' % (name, proj_id)
+            update_tasks = proj_inner.get('UPDATE')
+            if not update_tasks:
+                continue
 
-        task_recursion(space_id, proj['id'])
+            tasks = api.get_tasks(proj_id, is_root=True)
+
+            for pattern, task_inner in update_task.items():
+                for task_name, task_id in tasks.items():
+                    if not re.search(pattern, task_name):
+                        continue
+
+                    new_tasks = task_inner.get('NEW')
+                    if not new_tasks:
+                        continue
+
+                    for subtask in new_tasks:
+                        api.create_new_task(parent_id=task_id, space_id=space_id, **subtask)
+                        print 'Task: %r created!' % subtask['name']
+
+                        # invoke selenium
+
