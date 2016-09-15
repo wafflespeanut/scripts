7bdfd53d3120da837f0929969993899a08d4993f
Ravi Shankar <wafflespeanut@gmail.com>
2016-09-15 12:55:03 +0530

    Selenium automation for filling up the timesheet

--- python/asana_tasks.py
+++ python/asana_tasks.py
@@ -1,6 +1,15 @@
 from datetime import datetime
+from time import sleep
 
-import json, re, requests
+import json, os, re, requests, sys
+
+try:
+    from selenium import webdriver
+    from selenium.common.exceptions import NoSuchElementException
+except ImportError:
+    exit('Requires selenium webdriver! Install it using %r' % 'pip install selenium')
+
+CHROME_PATH = '/usr/lib/chromium-browser/chromedriver'
 
 
 class AsanaTasksCreator(object):
@@ -88,7 +97,102 @@ class AsanaTasksCreator(object):
         self._request('POST', url % parent_id, data)
 
 
+class GnatsFiller(object):
+    def __init__(self, username=None, password=None):
+        self.driver = webdriver.Chrome(CHROME_PATH)
+        self.driver.get("http://gnats.genomels.com/")
+        if username:
+            self.driver.find_element_by_xpath("//input[@id='user']").send_keys(username)
+        if password:
+            self.driver.find_element_by_xpath("//input[@id='password']").send_keys(password)
+        if username and password:
+            self.driver.find_element_by_xpath("//button[@type='submit']").click()
+
+        while not (username and password):
+            raw_input('Continue after logging in...')
+            if 'GLS Time Tracker' in self.driver.title:
+                break
+
+    def switch_date(self, date):
+        date_url = 'http://gnats.genomels.com/ts_asana.php?tdate=%s' % date
+        self.driver.get(date_url)
+
+    def loop_wait(self, func):
+        for i in xrange(1, sys.maxint):     # We won't usually go that far!
+            try:
+                func()
+                break
+            except NoSuchElementException:
+                sys.stdout.write('\rWaiting for page to finish loading... %d' % i)
+                sys.stdout.flush()
+                sleep(1)
+
+    def list_tasks(self, proj_id):
+        def inner():
+            self.driver.find_element_by_xpath("//a[contains(.,'GNA - Prod Dev')]").click()
+            sleep(1)
+            self.driver.find_element_by_xpath("//a[contains(@id,'%s')]" % proj_id).click()
+            # open additional tasks (without due-dates)
+            self.driver.find_element_by_xpath("/html/body/div[2]/aside[2]/section[2]/div/div[1]/div[2]/div[1]/div/button").click()
+
+        self.loop_wait(inner)
+
+    def list_subtasks(self, task_name):
+        def inner():
+            td = self.driver.find_element_by_xpath("//td[contains(.,' %s')]" % task_name)
+            tr = td.find_element_by_xpath('..')
+            tr.find_element_by_tag_name('i').click()
+
+        self.loop_wait(inner)
+
+    def pick_subtask(self, subtask_name):
+        def inner():
+            td = self.driver.find_element_by_xpath("//td[contains(.,' %s')]" % subtask_name)
+            tr = td.find_element_by_xpath('..')
+            tr.find_element_by_tag_name('button').click()
+            sleep(1)
+
+        self.loop_wait(inner)
+
+    def show_picked(self):
+        self.driver.find_element_by_xpath("//button[@id='todays']").click()
+
+    def add_custom(self, option, desc, hours):
+        self.driver.find_element_by_xpath("//button[@id='addfield']").click()
+        select = self.driver.find_element_by_xpath("//select[@name='category']")
+        tr = select.find_element_by_xpath('../..')
+        desc_inp = tr.find_element_by_xpath("//input[@name='descp']")
+        desc_inp.send_keys(desc)
+        hr_inp = tr.find_element_by_xpath("//input[@id='hours1']")
+        hr_inp.send_keys(hours)
+
+        options = select.find_elements_by_tag_name('option')
+        chosen_opt = filter(lambda opt: opt.get_attribute('value').lower() == option.lower(), options)
+        assert chosen_opt
+        chosen_opt[0].click()
+
+    def fill_picked(self, hours):
+        td = self.driver.find_element_by_xpath("//td[contains(.,'%s')]" % subtask_name)
+        tr = td.find_element_by_xpath('..')
+        tr.find_element_by_tag_name('input').send_keys(hours)
+
+    def finish(self, expect='09:00'):
+        def inner():
+            total_hours = self.driver.find_element_by_xpath("//center[@id='total_hours']").text
+            dt_hrs = datetime.strptime(total_hours, '%H:%M')
+            if dt_hrs < expect:
+                if raw_input('WARNING: Expected hours (%s) not fulfilled! Continue (y/n)? ' % expect) == 'y':
+                    raise StopIteration
+
+        self.driver.find_element_by_xpath("//button[@id='addupdate']")
+        expect = datetime.strptime(expect_hours, '%H:%M')
+        self.loop_wait(inner)
+
+
 def begin_battle(config):
+    if not os.path.exists(CHROME_PATH):
+        exit('Requires Chromedriver! Get it using %r' % 'sudo apt-get install chromium-chromedriver')
+
     api = AsanaTasksCreator(config['token'])
     space = config['space']
 
@@ -100,31 +204,6 @@ def begin_battle(config):
 
     print 'Workspace ID: %d' % (space)
     projects = api.get_projects()
+    tasks = config['tasks']
 
-    for name, proj_inner in config['projects'].items():
-        for proj_name, proj_id in projects.items():
-            if name != proj_name:
-                continue
-
-            print 'Project: %s (ID: %d)' % (name, proj_id)
-            update_tasks = proj_inner.get('UPDATE')
-            if not update_tasks:
-                continue
-
-            tasks = api.get_tasks(proj_id, is_root=True)
-
-            for pattern, task_inner in update_task.items():
-                for task_name, task_id in tasks.items():
-                    if not re.search(pattern, task_name):
-                        continue
-
-                    new_tasks = task_inner.get('NEW')
-                    if not new_tasks:
-                        continue
-
-                    for subtask in new_tasks:
-                        api.create_new_task(parent_id=task_id, space_id=space_id, **subtask)
-                        print 'Task: %r created!' % subtask['name']
-
-                        # invoke selenium
-
+    # let the siege begin!
