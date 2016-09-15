96c9740aff89e6f80b9c8370519042ff0d8be3e8
Ravi Shankar <wafflespeanut@gmail.com>
2016-09-15 19:09:11 +0530

    Working setup of automated task filler

--- python/asana_tasks.py
+++ python/asana_tasks.py
@@ -11,6 +11,26 @@ except ImportError:
 
 CHROME_PATH = '/usr/lib/chromium-browser/chromedriver'
 
+SAMPLE_CONFIG = {
+    "token": "foobar123",
+    "space": "genomels",
+
+    "gnats_user": "ravishankar@genomels.com",
+    "gnats_pass": 'SSdtIG5vdCB0aGF0IGR1bWIh'.decode('base64'),
+
+    "tasks": {
+        "YYYY-MM-DD": [
+            {
+                "type": "",     # "update", "custom", or "create"
+                "project": "",  # some project name (case insensitive)
+                "task": "",     # some task name or pattern
+                "subtask": "",  # some subtask name (subtasks are always created)
+                "hours": ""     # hours taken for this task
+            }
+        ]
+    }
+}
+
 
 class AsanaTasksCreator(object):
     '''
@@ -60,7 +80,7 @@ class AsanaTasksCreator(object):
         url = self.root_tasks_url if is_root else self.sub_tasks_url
         return self._get_stuff(url % parent_id)
 
-    def create_task_data(**kwargs):
+    def create_task_data(self, **kwargs):
         data = {}
         if kwargs.get('space_id'):
             data['workspace'] = kwargs['space_id']
@@ -82,22 +102,24 @@ class AsanaTasksCreator(object):
         if not data:
             return
 
-        url = self.tasks_url % task_id
-        self._request('PUT', task_id, data)
+        self._request('PUT', self.tasks_url % task_id, data)
 
     def create_new_task(self, parent_id, is_root=False, **kwargs):
-        if not (kwargs['name'] and kwargs['space_id']):
+        if not (kwargs.get('name') and kwargs.get('space_id')):
+            print 'WARNING: Name and Workspace IDs are required!'
             return
 
-        if is_root and not kwargs['proj_id']:
+        if is_root and not kwargs.get('proj_id'):
+            print 'WARNING: Project ID is required for creating root tasks'
             return
 
         data = self.create_task_data(**kwargs)
         url = self.root_tasks_url if is_root else self.sub_tasks_url
-        self._request('POST', url % parent_id, data)
+        return self._request('POST', url % parent_id, {'data': data})
 
 
 class GnatsFiller(object):
+    '''Selenium-based automation to get around my company's time tracker thing'''
     def __init__(self, username=None, password=None):
         self.driver = webdriver.Chrome(CHROME_PATH)
         self.driver.get("http://gnats.genomels.com/")
@@ -113,7 +135,7 @@ class GnatsFiller(object):
             if 'GLS Time Tracker' in self.driver.title:
                 break
 
-    def switch_date(self, date):
+    def switch_to_date(self, date):
         date_url = 'http://gnats.genomels.com/ts_asana.php?tdate=%s' % date
         self.driver.get(date_url)
 
@@ -121,6 +143,7 @@ class GnatsFiller(object):
         for i in xrange(1, sys.maxint):     # We won't usually go that far!
             try:
                 func()
+                print '\nFinished!'
                 break
             except NoSuchElementException:
                 sys.stdout.write('\rWaiting for page to finish loading... %d' % i)
@@ -157,7 +180,7 @@ class GnatsFiller(object):
     def show_picked(self):
         self.driver.find_element_by_xpath("//button[@id='todays']").click()
 
-    def add_custom(self, option, desc, hours):
+    def custom_add(self, option, desc, hours):
         self.driver.find_element_by_xpath("//button[@id='addfield']").click()
         select = self.driver.find_element_by_xpath("//select[@name='category']")
         tr = select.find_element_by_xpath('../..')
@@ -167,16 +190,23 @@ class GnatsFiller(object):
         hr_inp.send_keys(hours)
 
         options = select.find_elements_by_tag_name('option')
-        chosen_opt = filter(lambda opt: opt.get_attribute('value').lower() == option.lower(), options)
+        chosen_opt = filter(lambda opt: re.search(option.lower(), opt.get_attribute('value').lower()), options)
         assert chosen_opt
         chosen_opt[0].click()
 
-    def fill_picked(self, hours):
-        td = self.driver.find_element_by_xpath("//td[contains(.,'%s')]" % subtask_name)
-        tr = td.find_element_by_xpath('..')
-        tr.find_element_by_tag_name('input').send_keys(hours)
+    def fill_picked(self, subtask_name, hours):
+        def inner():
+            td = self.driver.find_element_by_xpath("//td[contains(.,'%s')]" % subtask_name)
+            tr = td.find_element_by_xpath('..')
+            dt_hrs = datetime.strptime(hours, '%H:%M').strftime('%H:%M')
+            tr.find_element_by_tag_name('input').send_keys(dt_hrs)
 
-    def finish(self, expect='09:00'):
+        self.loop_wait(inner)
+
+    def update_filled(self):
+        self.driver.find_element_by_xpath("//button[@id='addupdate']").click()
+
+    def finish(self, expect_hours='09:00'):
         def inner():
             total_hours = self.driver.find_element_by_xpath("//center[@id='total_hours']").text
             dt_hrs = datetime.strptime(total_hours, '%H:%M')
@@ -184,26 +214,104 @@ class GnatsFiller(object):
                 if raw_input('WARNING: Expected hours (%s) not fulfilled! Continue (y/n)? ' % expect) == 'y':
                     raise StopIteration
 
-        self.driver.find_element_by_xpath("//button[@id='addupdate']")
+        self.update_filled()
         expect = datetime.strptime(expect_hours, '%H:%M')
         self.loop_wait(inner)
 
 
 def begin_battle(config):
     if not os.path.exists(CHROME_PATH):
-        exit('Requires Chromedriver! Get it using %r' % 'sudo apt-get install chromium-chromedriver')
+        print 'Requires Chromedriver! Get it using %r' % 'sudo apt-get install chromium-chromedriver'
+        return
 
-    api = AsanaTasksCreator(config['token'])
+    asana = AsanaTasksCreator(config['token'])
     space = config['space']
 
     if isinstance(space, str):
-        spaces = api.get_workspaces()
-        for name, space in spaces.items():
-            if re.search(config['space'], name):
+        spaces = asana.get_workspaces()
+        spaces = filter(lambda (name, sp): re.search(config['space'], name), spaces.items())
+        if not spaces:
+            print 'Workspace not found!'
+            return
+        space = spaces[0][1]
+
+    print 'Workspace ID: %d' % space
+    projects = {}
+    fill_tasks = config['tasks']
+
+    for proj_name, proj_id in asana.get_projects().items():
+        proj = proj_name.lower()
+        projects[proj] = {}
+        projects[proj]['id'] = proj_id
+        projects[proj]['tasks'] = asana.get_tasks(proj_id, is_root=True)
+
+    username = config.get('gnats_user')
+    password = config.get('gnats_pass')
+    gnats = GnatsFiller(username, password)
+
+    for date, tasks_info in fill_tasks.items():
+        try:
+            date = datetime.strptime(date, '%Y-%m-%d').strftime('%Y-%m-%d')
+        except ValueError:
+            print 'Error parsing %r. Skipping task group...'
+            continue
+
+        tasks = []
+        update_tasks = []
+        custom_tasks = []
+
+        for task in tasks_info:
+            try:
+                datetime.strptime(task['hours'], '%H:%M').strftime('%H:%M')
+            except ValueError:
+                print 'Unable to parse hours (%s). Skipping %r...' % (task['hours'], task['subtask'])
+                continue
+
+            if task['type'] == 'custom':
+                custom_tasks.append(task)
+                continue
+
+            proj = task['project'].lower()
+            if not projects.get(proj):
+                print 'Project %r not found in Asana! Skipping %r...' % (proj, task['subtask'])
+                continue
+
+            if task['type'] == 'update':
+                update_tasks.append(task)
+
+        for task in update_tasks:
+            proj = projects[task['project'].lower()]
+            task_pattern = task['task']
+
+            for task_name, task_id in proj['tasks'].items():
+                if not re.search(task_pattern, task_name):
+                    continue
+
+                subtask_name = task['subtask']
+                print 'Creating new subtask %r...' % subtask_name
+                sub_task = asana.create_new_task(parent_id=task_id, space_id=space,
+                                                 name=subtask_name, assignee='me', date=date)
+                subtask_id = sub_task['id']
+
+                gnats.switch_to_date(date)
+                gnats.list_tasks(proj['id'])
+                gnats.list_subtasks(task_name)
+                gnats.pick_subtask(subtask_name)
+                gnats.show_picked()
+                gnats.fill_picked(subtask_name, task['hours'])
+                gnats.update_filled()
+
+                asana.update_task(subtask_id, completed=True)
+                print 'Marked %r as completed!' % subtask_name
+
                 break
 
-    print 'Workspace ID: %d' % (space)
-    projects = api.get_projects()
-    tasks = config['tasks']
+        for task in custom_tasks:
+            print 'Adding custom task... %r' % task['subtask']
+            gnats.show_picked()
+            gnats.custom_add(task['task'], task['subtask'], task['hours'])
+            gnats.update_filled()
 
-    # let the siege begin!
+
+    sleep(5)
+    gnats.driver.close()
