aa7325a99fdbec8c3b1a73107ad8a790eef8091d
Ravi Shankar <wafflespeanut@gmail.com>
2017-05-26 16:10:37 +0530

    Terminal raw mode (log on one side, stats on the other)

--- python/stock-watch.py
+++ python/stock-watch.py
@@ -2,17 +2,59 @@ from datetime import datetime
 from threading import Thread
 from time import sleep
 
-import sys, urllib2
+import fcntl, struct, sys, termios, tty, urllib2
 
-args = sys.argv[1:]
-name = args[0] if args else 'ashley'
-interval_secs = float(args[1]) if len(args) > 1 else 1
+class Watcher(object):
+    def __init__(self):
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        # reset with `termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)`
+        tty.setraw(sys.stdin)
+        self.llog = []
+        self.rlog = []
 
-url_1 = 'http://getquote.icicidirect.com/trading_stock_quote.aspx?Symbol=%s' % name
-url_2 = 'https://secure.icicidirect.com/IDirectTrading/trading/trading_stock_bestbid.aspx?Symbol=%s' % name
+    def update_size(self):
+        st = struct.pack('HHHH', 0, 0, 0, 0)
+        ret = fcntl.ioctl(0, termios.TIOCGWINSZ, st)
+        self.rows, self.columns, _hp, _wp = struct.unpack('HHHH', ret)
+        self.log_width = self.columns / 2 - 1
 
-last_share = 0
-filename = name + datetime.now().strftime('-%Y-%m-%d')
+    def set_line_right(self, idx, line):
+        while idx >= len(self.rlog):
+            self.rlog.append('')
+        self.rlog[idx] = line
+
+    def get_split_lines(self, lines):
+        chunked = []
+        for line in lines:
+            chunks = [line[i:i + self.log_width] for i in xrange(0, len(line), self.log_width)]
+            chunked.extend(chunks)
+        return chunked
+
+    def refresh(self):
+        self.update_size()
+        sys.stdout.write('\033[1;1H')
+        if len(self.llog) > self.rows:
+            self.llog = self.llog[-self.rows:]
+        if len(self.rlog) > self.rows:
+            self.rlog = self.rlog[:self.rows]
+
+        llines = self.get_split_lines(self.llog)
+        rlines = self.get_split_lines(self.rlog)
+
+        idx = self.rows - len(llines) - 1
+        for r in xrange(self.rows - 1):
+            if r < idx:
+                sys.stdout.write(' ' * self.log_width)
+            else:
+                line = llines[r - idx]
+                l = len(line)       # FIXME: account tab-formatted strings
+                sys.stdout.write(line + ' ' * (self.log_width - l))
+            sys.stdout.write(' | ')
+            if r < len(rlines):
+                sys.stdout.write(rlines[r])
+            sys.stdout.write('\r\n')
+
 
 # Thread that returns value on joining (https://stackoverflow.com/a/6894023/2313792)
 class ReturningThread(Thread):
@@ -28,7 +70,22 @@ class ReturningThread(Thread):
         Thread.join(self)
         return self._return
 
+
+args = sys.argv[1:]
+name = args[0] if args else 'ashley'
+interval_secs = float(args[1]) if len(args) > 1 else 1
+
+# url_1 = 'http://getquote.icicidirect.com/trading_stock_quote.aspx?Symbol=%s' % name
+# url_2 = 'https://secure.icicidirect.com/IDirectTrading/trading/trading_stock_bestbid.aspx?Symbol=%s' % name
+url_1 = 'file:///home/wafflespeanut/Desktop/trading_stock_quote.aspx?Symbol=ashley'
+url_2 = 'file:///home/wafflespeanut/Desktop/trading_stock_bestbid.aspx?Symbol=ashley'
+
+last_share = 0
+filename = name + datetime.now().strftime('-%Y-%m-%d')
+watcher = Watcher()
+
 while True:
+    watcher.refresh()
     try:
         thread = ReturningThread(target=lambda: urllib2.urlopen(url_2).read())
         thread.start()
@@ -49,14 +106,15 @@ while True:
     day_high, day_low = float(g('DAY HIGH')), float(g('DAY LOW'))
 
     if last_share:
-        line = '[%s]: %s\t%s\t%s\t%s\t(%s, %s)' % \
+        line = '[%s]: %s   %-5s %6s %10s  (%s, %s)' % \
                (g('DATE'), g('LAST TRADED TIME'), last_price, day_volume - last_share, day_volume, day_low, day_high)
-        print line
+        watcher.llog.append(line)
 
         idx = 116
-        for _ in range(5):      # 5 rows
+        for i in range(5):      # 5 rows
             end = idx + 7 * offset      # 7 cols
-            print '\t'.join(map(lambda i: l2[i], range(idx, end + 1, offset))[:4])
+            line = ' ' * 5 + ''.join(map(lambda i: '%-10s' % l2[i], range(idx, end + 1, offset))[:4])
+            watcher.set_line_right(i, line)
             idx = end + 5
 
         with open(filename, 'a') as fd:
