a6e4537afd3a17ade0ee9f743208ccdf85d2c9e4
Ravi Shankar <wafflespeanut@gmail.com>
2017-05-29 23:16:30 +0530

    Cover delivery calculations

--- python/stock-watch.py
+++ python/stock-watch.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from threading import Thread
 from time import sleep
 
-import argparse, fcntl, select, struct, sys, termios, tty, urllib2
+import fcntl, select, struct, sys, termios, tty, urllib2
 
 CURSOR_TOP_LEFT = '\033[1;1H'
 RED = '\033[91m'
@@ -86,9 +86,9 @@ parser.add_argument('--quantity', '-q', type=int, default=0,
                     help='total shares buy/sell (default: null)')
 parser.add_argument('--cost', '-c', type=float, default=0.0,
                     help='value of share during buy/sell time (default: null)')
-parser.add_argument('--type', '-t', choices=['buy', 'sell'], default='null',
+parser.add_argument('--type', '-t', choices=['buy', 'sell'], default=None,
                     help='buy/sell (default: null)')
-parser.add_argument('--day', '-d', choices=['intra', 'deliver'], default='null',
+parser.add_argument('--day', '-d', choices=['intra', 'deliver'], default=None,
                     help='day on which the trade will take place (default: null)')
 parser.add_argument('--interval', '-i', type=float, default=1,
                     help='sleep interval in seconds (default: 1)')
@@ -98,10 +98,8 @@ parser.add_argument('--output', '-o', action='store_true', default=False,
 args = parser.parse_args()
 name, amount, cost, interval_secs = args.market, args.quantity, args.cost, args.interval
 
-# url_1 = 'http://getquote.icicidirect.com/trading_stock_quote.aspx?Symbol=%s' % name
-# url_2 = 'https://secure.icicidirect.com/IDirectTrading/trading/trading_stock_bestbid.aspx?Symbol=%s' % name
-url_1 = 'file:///home/wafflespeanut/Desktop/trading_stock_quote.aspx?Symbol=ashley'
-url_2 = 'file:///home/wafflespeanut/Desktop/trading_stock_bestbid.aspx?Symbol=ashley'
+url_1 = 'http://getquote.icicidirect.com/trading_stock_quote.aspx?Symbol=%s' % name
+url_2 = 'https://secure.icicidirect.com/IDirectTrading/trading/trading_stock_bestbid.aspx?Symbol=%s' % name
 
 last_share = 0
 filename = name + datetime.now().strftime('-%Y-%m-%d')
@@ -127,11 +125,14 @@ while True:
     last_price = float(g('LAST TRADE PRICE'))
     day_volume = int(''.join(g('DAY VOLUME').split(',')))
     day_high, day_low = float(g('DAY HIGH')), float(g('DAY LOW'))
+    date, last_time = g('DATE'), g('LAST TRADED TIME')
 
     if last_share:
         line = '[%s]: %s   %-5s %6s %10s  (%s, %s)' % \
                (g('DATE'), g('LAST TRADED TIME'), last_price, day_volume - last_share, day_volume, day_low, day_high)
         watcher.llog.append(line)
+
+        write_line = '\t'.join(map(str, [date, last_time, last_price, day_volume - last_share, day_volume, day_low, day_high]))
 
         idx = 116
         max_len = 0
@@ -139,11 +140,17 @@ while True:
         rows, cols = 5, 7
         for i in range(rows):
             end = idx + cols * offset
-            line = ' ' * x_shift + ''.join(map(lambda i: '%-10s' % l2[i], range(idx, end + 1, offset))[:4])
+            result = map(lambda i: l2[i], range(idx, end + 1, offset))[:4]
+            write_line += '\t' + '\t'.join(result)
+            line = ' ' * x_shift + ''.join(map(lambda v: '%-10s' % v, result))
             if len(line) > max_len:
                 max_len = len(line)
             watcher.set_line_right(i + y_shift + 1, line)
             idx = end + 5
+
+        if args.output:
+            with open(filename, 'a') as fd:
+                fd.write(write_line + '\n')
 
         watcher.set_line_right(y_shift - 1, ' ' * x_shift + '-' * (max_len - x_shift - 5))
         watcher.set_line_right(rows + y_shift + 2, ' ' * x_shift + '-' * (max_len - x_shift - 5))
@@ -153,11 +160,13 @@ while True:
         y_shift = watcher.rows / 3 + 3
         opp_act = 'buy' if args.type == 'sell' else 'sell' if args.type == 'buy' else 'null'
 
+        trade_worth = amount * cost
+        curr_worth = amount * last_price
+        diff = (last_price - cost) if args.type == 'buy' else (cost - last_price)
+        stuff = [('Stock worth', trade_worth)]
+
         if args.day == 'intra':
             intra_frac = 0.06
-            trade_worth = amount * cost
-            curr_worth = amount * last_price
-            diff = (last_price - cost) if args.type == 'buy' else (cost - last_price)
             blocked = trade_worth * intra_frac
             brokerage = (trade_worth + curr_worth) * 0.0005
             tax = brokerage * 0.14
@@ -165,29 +174,49 @@ while True:
             stamp = (trade_worth + curr_worth) * 0.00006
             sebi = (trade_worth + curr_worth) * (0.00002 + 0.0000231)
             net_comm = brokerage + tax + stt + stamp + sebi
-            final = curr_worth - net_comm
             earned = amount * diff
             on_hand = earned - net_comm
-            res = 'gain' if on_hand > 0 else 'loss' if on_hand < 0 else 'neutral'
+            stuff.extend([('Current worth', curr_worth), ('Blocked amount', blocked), ('Cash earned', earned),
+                          ('Commission', -net_comm), ('Cash on hand', on_hand)])
+        elif args.day == 'deliver':
+            if args.type == 'sell':
+                exit('Only buying is allowed for delivery!')
 
+            brokerage = trade_worth * 0.0055
+            tax = brokerage * 0.14
+            stt = trade_worth * 0.001
+            sebi = trade_worth * 0.000002
+            transact = trade_worth * 0.000031
+            stamp = trade_worth * 0.0001
+            net_comm = brokerage + tax + stt + sebi + transact + stamp
+            blocked = trade_worth + net_comm
+            stuff.extend([('Commission (buy)', net_comm), ('Vanished amount', blocked)])
+
+            brokerage = curr_worth * 0.0055
+            tax = brokerage * 0.14
+            stt = curr_worth * 0.001
+            sebi = curr_worth * 0.000002
+            transact = curr_worth * 0.000031
+            stamp = curr_worth * 0.0001
+            net_comm = brokerage + tax + stt + sebi + transact + stamp
+            earned = curr_worth - net_comm
+            on_hand = earned - blocked
+            stuff.extend([('Current worth', curr_worth), ('Commission (sell)', -net_comm),
+                          ('Cash earned', earned), ('Cash on hand', on_hand)])
+
+        if args.day:
+            res = 'gain' if on_hand > 0 else 'loss' if on_hand < 0 else 'neutral'
             watcher.set_line_right(y_shift,
                                    name.upper() + \
-                                   ' (%s) %s%s%s' % \
-                                   (args.day.title(), RED if res == 'loss' else GREEN, '[%s]' % res.upper(), COLOR_NULL))
+                                   ' (%s) %s%s%s %10.2f' % \
+                                   (args.day.title(), RED if res == 'loss' else GREEN, '[%s]' % res.upper(), COLOR_NULL,
+                                    on_hand))
             watcher.set_line_right(y_shift + 2, '%-4s %s at %s' % (args.type.upper(), amount, cost))
             watcher.set_line_right(y_shift + 3, '%-4s %s at %s' % (opp_act.upper(), amount, last_price))
             watcher.set_line_right(y_shift + 5, 'DIFF: %s' % diff)
 
-            stuff = [('Stock worth', trade_worth), ('Current worth', curr_worth), ('Blocked amount', blocked),
-                     ('Cash earned', earned), ('Commission', -net_comm), ('Cash on hand', on_hand)]
             for i, (label, value) in enumerate(stuff):
-                watcher.set_line_right(y_shift + 7 + i, '%-15s : %10.2f' % (label, value))
-        elif args.day == 'deliver':
-            pass
-
-        if args.output:
-            with open(filename, 'a') as fd:
-                fd.write(line + '\n')
+                watcher.set_line_right(y_shift + 7 + i, '%-18s : %10.2f' % (label, value))
 
     last_share = day_volume
     inp, out, err = select.select([sys.stdin], [], [], interval_secs)       # listens to key input in the sleep interval
